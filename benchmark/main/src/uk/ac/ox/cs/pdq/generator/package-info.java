d97979bd447bba53053b86b2c85f0dd58e8f0f49
tsamoura <tsamoura@cs.ox.ac.uk>
2016-01-29 14:04:21 +0000

    Fixing docs in benchmark

--- benchmark/main/src/uk/ac/ox/cs/pdq/generator/package-info.java
+++ benchmark/main/src/uk/ac/ox/cs/pdq/generator/package-info.java
@@ -4,17 +4,10 @@ package uk.ac.ox.cs.pdq.generator;
  * 
  * This package contains top-level interfaces for query, view and dependency generators.
  * 
- * The package first defines a generator which creates tuple generating dependencies given an input query.
- * In brief the generation process proceeds as follows:
- * The generator creates the relations of the schema along with their access methods. 
- * Second, given the schema relations, the generator creates the query and, finally, the dependencies.
- * It supports the creation of guarded queries (queries having a guard in their body), 
- * chain guarded queries (a chain query with a guard) and acyclic queries.
+ * The tgdsfromquery defines a generator which creates tuple generating dependencies given an input query conjunctive query.
  * 
- * The package second creates inclusion dependencies and then queries using the previously created dependencies.
+ * The packages queryfromids and queryfromids2 create first a schema with inclusion dependencies and then a query using the schema dependencies as a pivot.
  * 
- * The package third creates inclusion dependencies and then queries using the previously created dependencies.
- *  
  * 
  * @author Efthymia Tsamoura
  */
