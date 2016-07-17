d9f2c4c7ef593ea06cf60c0836f5515b257e112c
georgeKon <konstant@usc.edu>
2016-07-17 13:51:48 +0100

    Forgoto to commit one file for #149, #150, #151. Still missing a few comments.

---
+++ reasoning/main/src/uk/ac/ox/cs/pdq/reasoning/homomorphism/TriggerProperty.java
@@ -0,0 +1,14 @@
+package uk.ac.ox.cs.pdq.reasoning.homomorphism;
+
+
+/**
+ * Enumeration of different options for chase triggers
+ *
+ * @author George K
+ */
+public  enum TriggerProperty {
+	
+	ACTIVE,
+	ALL;
+
+}
