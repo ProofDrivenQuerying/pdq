227a5999f36d77b254ae6b4123eabae75e930f2c
Gabor <Gabor@192.168.18.46>
2017-08-02 17:03:06 +0100

    Comment improved

--- common/main/src/uk/ac/ox/cs/pdq/io/jaxb/package-info.java
+++ common/main/src/uk/ac/ox/cs/pdq/io/jaxb/package-info.java
@@ -1,8 +1,15 @@
 /**
- * Since most classes are immutable traditional bean-type building of those
- * classes is not possible. The classes in the "adapted" sub-package bypass this
- * problem by having setters and getters for the parameters necessary to create
- * the immutable version of the class.
+ * The io.jaxb package will replace the old io.xml package with a jaxb xml
+ * parser.
+ * 
+ * This implementation has minimal impact on the Exported objects, usually the
+ * only change is some added annotations.
+ * 
+ * In some other cases we have to create wrapper objects because immutable
+ * classes cannot be used in a traditional bean-type xml-building. The classes
+ * in the "adapted" sub-package bypass this problem by having setters and
+ * getters for the parameters necessary to create the immutable version of the
+ * class.
  * 
  * The classes in the adapters sub-package will convert between the original and
  * the adapted versions.
