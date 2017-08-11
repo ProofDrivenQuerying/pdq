ec76f14ddc8a2c8bf24e9599dc6552d919e6bf4a
Gabor <Gabor@192.168.18.46>
2017-08-11 16:15:32 +0100

    Schema creation and droping in case of derby fixed. Also I accidentally formatted the file.

--- common/main/src/uk/ac/ox/cs/pdq/db/DatabaseConnection.java
+++ common/main/src/uk/ac/ox/cs/pdq/db/DatabaseConnection.java
@@ -15,40 +15,47 @@ import uk.ac.ox.cs.pdq.db.sql.DerbyStatementBuilder;
 import uk.ac.ox.cs.pdq.db.sql.MySQLStatementBuilder;
 import uk.ac.ox.cs.pdq.db.sql.PostgresStatementBuilder;
 import uk.ac.ox.cs.pdq.db.sql.SQLStatementBuilder;
+
 /**
- * Models a database connection. It is responsible for creating database tables for the input schema.
+ * Models a database connection. It is responsible for creating database tables
+ * for the input schema.
  * 
  *
  */
-public class DatabaseConnection implements AutoCloseable{
+public class DatabaseConnection implements AutoCloseable {
 	private int synchronousThreadsNumber = 1;
 
 	private static Integer counter = 0;
 
 	private boolean isInitialized = false;
 
-	/**  Open database connections. */
-	//TOCOMMENT: this should be done with org.apache.commons.dbcp2 connection pooling.
+	/** Open database connections. */
+	// TOCOMMENT: this should be done with org.apache.commons.dbcp2 connection
+	// pooling.
 	protected List<Connection> synchronousConnections = Lists.newArrayList();
 
 	/** Map schema relation to database tables. */
 	private Map<String, Relation> relationNamesToDatabaseTables = null;
 
-	/**  Creates SQL statements to detect homomorphisms or add/delete facts in a database. */
+	/**
+	 * Creates SQL statements to detect homomorphisms or add/delete facts in a
+	 * database.
+	 */
 	private final SQLStatementBuilder builder;
 
 	private final DatabaseParameters databaseParameters;
 
 	private final Schema schema;
+
 	public DatabaseConnection(DatabaseParameters databaseParameters, Schema schema) throws SQLException {
-		this(databaseParameters, schema,1);
+		this(databaseParameters, schema, 1);
 	}
 
-	public DatabaseConnection(DatabaseParameters databaseParameters, Schema schema,int numberOfSynchConn) throws SQLException {
+	public DatabaseConnection(DatabaseParameters databaseParameters, Schema schema, int numberOfSynchConn) throws SQLException {
 		this.synchronousThreadsNumber = numberOfSynchConn;
 		String driver = databaseParameters.getDatabaseDriver();
 		String url = databaseParameters.getConnectionUrl();
-		String database = databaseParameters.getDatabaseName(); 
+		String database = databaseParameters.getDatabaseName();
 		String username = databaseParameters.getDatabaseUser();
 		String password = databaseParameters.getDatabasePassword();
 		if (url != null && url.contains("mysql")) {
@@ -56,16 +63,19 @@ public class DatabaseConnection implements AutoCloseable{
 		} else if (url != null && url.contains("postgres")) {
 			this.builder = new PostgresStatementBuilder();
 		} else {
+
 			if (Strings.isNullOrEmpty(driver)) {
 				driver = "org.apache.derby.jdbc.EmbeddedDriver";
 			}
 			if (Strings.isNullOrEmpty(url)) {
-				url = "jdbc:derby:memory:{1};create=true";
+				url = "jdbc:derby:memory:tmp;create=true";
 			}
 			if (Strings.isNullOrEmpty(database)) {
 				database = "chase";
 			}
-			database +=  "_" + System.currentTimeMillis() + "_" + counter++;
+			database += "_" + System.currentTimeMillis() + "_" + counter++;
+			database = database.toUpperCase();
+			databaseParameters.setDatabaseName(database);
 			synchronized (counter) {
 				username = "APP_" + (counter++);
 			}
@@ -73,9 +83,13 @@ public class DatabaseConnection implements AutoCloseable{
 			this.builder = new DerbyStatementBuilder();
 		}
 
-		for(int j=0; j<synchronousThreadsNumber; j++)
+		for (int j = 0; j < synchronousThreadsNumber; j++)
 			this.synchronousConnections.add(DatabaseUtilities.getConnection(driver, url, database, username, password));
 
+		if (driver.contains("derby")) {
+			Statement st = this.synchronousConnections.get(0).createStatement();
+			st.execute("create schema " + database);
+		}
 		this.schema = schema;
 		this.databaseParameters = databaseParameters;
 		this.relationNamesToDatabaseTables = new LinkedHashMap<>();
@@ -91,19 +105,20 @@ public class DatabaseConnection implements AutoCloseable{
 
 	/**
 	 * Sets up the database that will store the facts.
-	 * @throws SQLException 
+	 * 
+	 * @throws SQLException
 	 */
 	protected void setup() throws SQLException {
 		Statement sqlStatement = null;
 		List<String> commandBuffer = new ArrayList<String>();
 		try {
 			sqlStatement = this.synchronousConnections.get(0).createStatement();
-			for (String sql: this.builder.createDatabaseStatements(this.databaseParameters.getDatabaseName())) {
+			for (String sql : this.builder.createDatabaseStatements(this.databaseParameters.getDatabaseName())) {
 				commandBuffer.add(sql);
 				sqlStatement.addBatch(sql);
 			}
-			//Create the database tables and create column indices
-			for (Relation relation:this.schema.getRelations()) {
+			// Create the database tables and create column indices
+			for (Relation relation : this.schema.getRelations()) {
 				Relation dbRelation = this.createDatabaseRelation(relation);
 				this.relationNamesToDatabaseTables.put(relation.getName(), dbRelation);
 				String command = this.builder.createTableStatement(dbRelation);
@@ -111,21 +126,26 @@ public class DatabaseConnection implements AutoCloseable{
 				commandBuffer.add(command);
 			}
 			sqlStatement.executeBatch();
-		}catch(Throwable t) {
-			if (sqlStatement!=null) {
+		} catch (Throwable t) {
+			if (sqlStatement != null) {
 				System.err.println("SQL warnings: " + sqlStatement.getWarnings());
 				System.err.println("Batch commands: " + commandBuffer);
 			}
 			t.printStackTrace();
-			//((java.sql.BatchUpdateException)t).getNextException()
+			// ((java.sql.BatchUpdateException)t).getNextException()
 			throw t;
+		} finally {
+			if (sqlStatement != null)
+				sqlStatement.close();
 		}
 	}
 
 	/**
-	 * Creates the db relation. Currently codes in the position numbers into the names, but this should change
+	 * Creates the db relation. Currently codes in the position numbers into the
+	 * names, but this should change
 	 *
-	 * @param relation the relation
+	 * @param relation
+	 *            the relation
 	 * @return a new database relation with attributes x0,x1,...,x_{N-1}, Fact where
 	 *         x_i maps to the i-th relation's attribute
 	 */
@@ -134,9 +154,9 @@ public class DatabaseConnection implements AutoCloseable{
 		Attribute[] attributes = new Attribute[relation.getArity()];
 		for (int index = 0; index < relation.getArity(); index++) {
 			Attribute attribute = relation.getAttribute(index);
-			if (Integer.class.isAssignableFrom((Class<?>) attribute.getType()) && attribute.getName().equals("InstanceID")) 
+			if (Integer.class.isAssignableFrom((Class<?>) attribute.getType()) && attribute.getName().equals("InstanceID"))
 				attributes[index] = Attribute.create(Integer.class, "InstanceID");
-			else 
+			else
 				attributes[index] = Attribute.create(String.class, attrPrefix + index);
 		}
 		return Relation.create(relation.getName(), attributes, relation.isEquality());
@@ -145,38 +165,52 @@ public class DatabaseConnection implements AutoCloseable{
 	/**
 	 * Cleans up the database.
 	 *
-	 * @throws HomomorphismException the homomorphism exception
-	 * @throws SQLException 
+	 * @throws HomomorphismException
+	 *             the homomorphism exception
+	 * @throws SQLException
 	 */
 	protected void dropDatabase() throws SQLException {
 		Statement sqlStatement = this.synchronousConnections.get(0).createStatement();
-		for (String sql: this.builder.createDropStatements(this.databaseParameters.getDatabaseName())) 
+		for (String sql : this.builder.createDropStatements(this.databaseParameters.getDatabaseName()))
 			sqlStatement.addBatch(sql);
 		sqlStatement.executeBatch();
+		sqlStatement.close();
 	}
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.AutoCloseable#close()
 	 */
 	@Override
 	public void close() throws Exception {
-		this.dropDatabase();
-		for(Connection connection:this.synchronousConnections) 
-			connection.close();
+		try {
+			this.dropDatabase();
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+		try {
+			for (Connection connection : this.synchronousConnections)
+				connection.close();
+			synchronousConnections.clear();
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
 	}
 
 	@Override
 	public DatabaseConnection clone() {
 		try {
-			DatabaseConnection cloneCon = new DatabaseConnection(this.databaseParameters,this.schema);
+			DatabaseConnection cloneCon = new DatabaseConnection(this.databaseParameters, this.schema);
 			cloneCon.isInitialized = this.isInitialized;
 			return cloneCon;
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
+
 	/**
-	 * Map from relation names to the main memory objects existing for these names. 
+	 * Map from relation names to the main memory objects existing for these names.
 	 * TOCOMMENT: See issue 168
 	 */
 	public Map<String, Relation> getRelationNamesToDatabaseTables() {
@@ -190,20 +224,21 @@ public class DatabaseConnection implements AutoCloseable{
 	public Schema getSchema() {
 		return this.schema;
 	}
-	
+
 	public List<Connection> getSynchronousConnections() {
 		return this.synchronousConnections;
 	}
-	
+
 	public Connection getSynchronousConnections(int index) {
 		return this.synchronousConnections.get(index);
 	}
-	
+
 	public DatabaseParameters getDatabaseParameters() {
 		return this.databaseParameters;
 	}
-	
+
 	public int getNumberOfSynchronousConnections() {
 		return this.synchronousConnections.size();
 	}
+
 }
