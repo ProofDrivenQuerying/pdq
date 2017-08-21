c6b16985294cccdc2331c8bbaefc682abdc4af7d
Gabor <Gabor@192.168.18.46>
2017-08-21 15:27:53 +0100

    Converter utility to create new schema xml from old schema xml

---
+++ datasources/main/src/uk/ac/ox/cs/pdq/datasources/utility/SchemaConverter.java
@@ -0,0 +1,34 @@
+package uk.ac.ox.cs.pdq.datasources.utility;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import javax.xml.bind.JAXBException;
+
+import uk.ac.ox.cs.pdq.datasources.io.jaxb.DbIOManager;
+import uk.ac.ox.cs.pdq.db.Schema;
+
+public class SchemaConverter {
+
+	public static void convert(File from, File to) throws FileNotFoundException, JAXBException {
+		Schema schema = DbIOManager.importSchema(from);
+		if (to.isDirectory()) {
+			DbIOManager.exportSchemaToXml(schema, new File(to,from.getName()));
+		} else {
+			DbIOManager.exportSchemaToXml(schema, to);
+		}
+	}
+	
+	public static void main(String[] args) {
+		File to = new File("C:\\Work\\converted");
+		
+		File src = new File("c:\\Users\\Gabor\\git\\pdq\\regression\\test\\linear\\fast\\demo\\derby\\case_001\\schema.xml");
+		try {
+			convert(src,to);
+		} catch (FileNotFoundException | JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
