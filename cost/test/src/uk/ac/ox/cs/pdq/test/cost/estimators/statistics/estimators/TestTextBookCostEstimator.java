b8ea4f25e36da776ee267901ea6fc284d41b7870
Gabor <Gabor@192.168.18.46>
2017-08-30 16:03:29 +0100

    Assertions for current cost added for reference. Test still need to be compaired with review

--- cost/test/src/uk/ac/ox/cs/pdq/test/cost/estimators/statistics/estimators/TestTextBookCostEstimator.java
+++ cost/test/src/uk/ac/ox/cs/pdq/test/cost/estimators/statistics/estimators/TestTextBookCostEstimator.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.when;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -38,70 +39,71 @@ public class TestTextBookCostEstimator {
 	protected Attribute[] attributes18Old = new Attribute[18];
 	protected Attribute[] attributes21Old = new Attribute[21];
 	protected Attribute[] newAttributes = new Attribute[30];
-	
-	protected AccessMethod method0 = AccessMethod.create(new Integer[]{});
-	protected AccessMethod method1 = AccessMethod.create(new Integer[]{0});
-	protected AccessMethod method2 = AccessMethod.create(new Integer[]{0,1});
-	protected AccessMethod method3 = AccessMethod.create(new Integer[]{1});
-	
+
+	protected AccessMethod method0 = AccessMethod.create(new Integer[] {});
+	protected AccessMethod method1 = AccessMethod.create(new Integer[] { 0 });
+	protected AccessMethod method2 = AccessMethod.create(new Integer[] { 0, 1 });
+	protected AccessMethod method3 = AccessMethod.create(new Integer[] { 1 });
+
 	protected Relation YahooPlaceCode;
 	protected Relation YahooPlaceRelationship;
 	protected Relation YahooWeather;
 	protected Relation YahooPlaces;
-	
+
 	protected Attribute a = Attribute.create(Integer.class, "a");
 	protected Attribute b = Attribute.create(Integer.class, "b");
 	protected Attribute c = Attribute.create(Integer.class, "c");
 	protected Attribute d = Attribute.create(Integer.class, "d");
-    
+
 	protected Relation R;
-	protected Relation S;	
-	
+	protected Relation S;
+
 	@Mock
 	protected SimpleCatalog catalog;
-    
+
 	/**
 	 * Setup.
 	 */
-	@Before public void setup() {
+	@Before
+	public void setup() {
 		Utility.assertsEnabled();
-        MockitoAnnotations.initMocks(this);
-        
-        for(int index = 0; index < attributes3Old.length; ++index) 
-        	this.attributes3Old[index] = Attribute.create(String.class, "a" + index);
-        
-        for(int index = 0; index < attributes6Old.length; ++index) 
-        	this.attributes6Old[index] = Attribute.create(String.class, "a" + index);
-        
-        for(int index = 0; index < attributes18Old.length; ++index) 
-        	this.attributes18Old[index] = Attribute.create(String.class, "a" + index);
-        
-        for(int index = 0; index < attributes21Old.length; ++index) 
-        	this.attributes21Old[index] = Attribute.create(String.class, "a" + index);
-        
-        for(int index = 0; index < newAttributes.length; ++index) 
-        	this.newAttributes[index] = Attribute.create(String.class, "c" + index);
-        
-    
-        this.YahooPlaceCode = Relation.create("YahooPlaceCode", this.attributes3Old, new AccessMethod[]{this.method2});
-        this.YahooPlaceRelationship = Relation.create("YahooPlaceRelationship", this.attributes6Old, new AccessMethod[]{this.method2});
-        this.YahooWeather = Relation.create("YahooWeather", this.attributes21Old, new AccessMethod[]{this.method1});
-        this.YahooPlaces = Relation.create("YahooPlaces", this.attributes18Old, new AccessMethod[]{this.method3});
-    	
-        //Create the mock catalog object
-        when(this.catalog.getCardinality(this.YahooPlaceCode)).thenReturn(10000000);
-        when(this.catalog.getCardinality(this.YahooPlaceRelationship)).thenReturn(10000000);
-        when(this.catalog.getCardinality(this.YahooWeather)).thenReturn(100000000);
-        when(this.catalog.getCardinality(this.YahooPlaces)).thenReturn(10000000);
-        
-        this.R = Relation.create("R", new Attribute[]{a,b,c}, new AccessMethod[]{this.method0, this.method2});
-        this.S = Relation.create("S", new Attribute[]{b,c}, new AccessMethod[]{this.method0, this.method1, this.method2});
-        //Create the mock catalog object
-        when(this.catalog.getCardinality(this.R)).thenReturn(100);
-        when(this.catalog.getCardinality(this.S)).thenReturn(100);
+		MockitoAnnotations.initMocks(this);
+
+		for (int index = 0; index < attributes3Old.length; ++index)
+			this.attributes3Old[index] = Attribute.create(String.class, "a" + index);
+
+		for (int index = 0; index < attributes6Old.length; ++index)
+			this.attributes6Old[index] = Attribute.create(String.class, "a" + index);
+
+		for (int index = 0; index < attributes18Old.length; ++index)
+			this.attributes18Old[index] = Attribute.create(String.class, "a" + index);
+
+		for (int index = 0; index < attributes21Old.length; ++index)
+			this.attributes21Old[index] = Attribute.create(String.class, "a" + index);
+
+		for (int index = 0; index < newAttributes.length; ++index)
+			this.newAttributes[index] = Attribute.create(String.class, "c" + index);
+
+		this.YahooPlaceCode = Relation.create("YahooPlaceCode", this.attributes3Old, new AccessMethod[] { this.method2 });
+		this.YahooPlaceRelationship = Relation.create("YahooPlaceRelationship", this.attributes6Old, new AccessMethod[] { this.method2 });
+		this.YahooWeather = Relation.create("YahooWeather", this.attributes21Old, new AccessMethod[] { this.method1 });
+		this.YahooPlaces = Relation.create("YahooPlaces", this.attributes18Old, new AccessMethod[] { this.method3 });
+
+		// Create the mock catalog object
+		when(this.catalog.getCardinality(this.YahooPlaceCode)).thenReturn(10000000);
+		when(this.catalog.getCardinality(this.YahooPlaceRelationship)).thenReturn(10000000);
+		when(this.catalog.getCardinality(this.YahooWeather)).thenReturn(100000000);
+		when(this.catalog.getCardinality(this.YahooPlaces)).thenReturn(10000000);
+
+		this.R = Relation.create("R", new Attribute[] { a, b, c }, new AccessMethod[] { this.method0, this.method2 });
+		this.S = Relation.create("S", new Attribute[] { b, c }, new AccessMethod[] { this.method0, this.method1, this.method2 });
+		// Create the mock catalog object
+		when(this.catalog.getCardinality(this.R)).thenReturn(100);
+		when(this.catalog.getCardinality(this.S)).thenReturn(100);
 	}
-	
-	@Test public void test1() {
+
+	@Test
+	public void test1() {
 		Map<Integer, TypedConstant> inputConstants1 = new HashMap<>();
 		inputConstants1.put(0, TypedConstant.create("iso"));
 		inputConstants1.put(1, TypedConstant.create("FR"));
@@ -110,103 +112,106 @@ public class TestTextBookCostEstimator {
 		inputConstants2.put(0, TypedConstant.create("children"));
 		AccessTerm access2 = AccessTerm.create(this.YahooPlaceRelationship, this.method2, inputConstants2);
 		AccessTerm access3 = AccessTerm.create(this.YahooWeather, this.method1);
-		
-		Attribute[] renamings1 = new Attribute[]{Attribute.create(String.class, "dummy1"), Attribute.create(String.class, "dummy2"), Attribute.create(String.class, "c1")};
+
+		Attribute[] renamings1 = new Attribute[] { Attribute.create(String.class, "dummy1"), Attribute.create(String.class, "dummy2"), Attribute.create(String.class, "c1") };
 		Attribute[] renamings2 = new Attribute[6];
 		System.arraycopy(this.newAttributes, 1, renamings2, 1, 5);
 		renamings2[0] = Attribute.create(String.class, "dummy3");
 		Attribute[] renamings3 = new Attribute[21];
 		System.arraycopy(this.newAttributes, 6, renamings3, 1, 20);
 		renamings3[0] = Attribute.create(String.class, "c2");
-		
+
 		RenameTerm rename1 = RenameTerm.create(renamings1, access1);
 		RenameTerm rename2 = RenameTerm.create(renamings2, access2);
 		RenameTerm rename3 = RenameTerm.create(renamings3, access3);
-		
+
 		DependentJoinTerm plan1 = DependentJoinTerm.create(rename1, rename2);
 		DependentJoinTerm plan2 = DependentJoinTerm.create(plan1, rename3);
-		
+
 		NaiveCardinalityEstimator cardinalityEstimator = new NaiveCardinalityEstimator(this.catalog);
 		TextBookCostEstimator estimator = new TextBookCostEstimator(null, cardinalityEstimator);
 		Cost cost = estimator.cost(plan2);
-		
-		//TODO add assertions for cost  
+		Assert.assertEquals(3.3958007884803287E18, (double) cost.getValue(), 0.0001);
 	}
-	
-	@Test public void test2() {
+
+	@Test
+	public void test2() {
 		Map<Integer, TypedConstant> inputConstants1 = new HashMap<>();
 		inputConstants1.put(1, TypedConstant.create("Eiffel Tower"));
 		AccessTerm access1 = AccessTerm.create(this.YahooPlaces, this.method3, inputConstants1);
-		
+
 		NaiveCardinalityEstimator cardinalityEstimator = new NaiveCardinalityEstimator(this.catalog);
 		TextBookCostEstimator estimator = new TextBookCostEstimator(null, cardinalityEstimator);
 		Cost cost = estimator.cost(access1);
-		
-		//TODO add assertions for cost 
+
+		Assert.assertEquals(2.4867919004335693E8, (double) cost.getValue(), 0.0001);
 	}
-	
-	@Test public void test3() {
+
+	@Test
+	public void test3() {
 		AccessTerm access1 = AccessTerm.create(this.R, this.method0);
 		AccessTerm access2 = AccessTerm.create(this.S, this.method1);
 		DependentJoinTerm plan1 = DependentJoinTerm.create(access1, access2);
-		
+
 		NaiveCardinalityEstimator cardinalityEstimator = new NaiveCardinalityEstimator(this.catalog);
 		TextBookCostEstimator estimator = new TextBookCostEstimator(null, cardinalityEstimator);
 		Cost cost = estimator.cost(plan1);
-		
-		//TODO add assertions for cost 
+
+		Assert.assertEquals(6186.72124178452, (double) cost.getValue(), 0.0001);
 	}
-	
-	
-	@Test public void test4() {
+
+	@Test
+	public void test4() {
 		AccessTerm access1 = AccessTerm.create(this.R, this.method0);
-		AccessTerm access2 = AccessTerm.create(this.S, this.method2);		
+		AccessTerm access2 = AccessTerm.create(this.S, this.method2);
 		DependentJoinTerm plan1 = DependentJoinTerm.create(access1, access2);
-		
-		NaiveCardinalityEstimator cardinalityEstimator = new NaiveCardinalityEstimator(this.catalog);					
+
+		NaiveCardinalityEstimator cardinalityEstimator = new NaiveCardinalityEstimator(this.catalog);
 		TextBookCostEstimator estimator = new TextBookCostEstimator(null, cardinalityEstimator);
 		Cost cost = estimator.cost(plan1);
-		
-		//TODO add assertions for cost 
+
+		Assert.assertEquals(1781.5510557964276, (double) cost.getValue(), 0.0001);
 	}
-	
-	@Test public void test5() {
+
+	@Test
+	public void test5() {
 		AccessTerm access1 = AccessTerm.create(this.R, this.method0);
 		AccessTerm access2 = AccessTerm.create(this.S, this.method2);
 		SelectionTerm selectionTerm = SelectionTerm.create(ConstantEqualityCondition.create(0, TypedConstant.create(new Integer(1))), access1);
 		DependentJoinTerm plan1 = DependentJoinTerm.create(selectionTerm, access2);
-		
-		NaiveCardinalityEstimator cardinalityEstimator = new NaiveCardinalityEstimator(this.catalog);		
+
+		NaiveCardinalityEstimator cardinalityEstimator = new NaiveCardinalityEstimator(this.catalog);
 		TextBookCostEstimator estimator = new TextBookCostEstimator(null, cardinalityEstimator);
 		Cost cost = estimator.cost(plan1);
-		
-		//TODO add assertions for cost
+		Assert.assertEquals(1431.5510557964276, (double) cost.getValue(), 0.0001);
 	}
-	
-	@Test public void test6() {	
+
+	@Test
+	public void test6() {
 		Map<Integer, TypedConstant> inputConstants1 = new HashMap<>();
 		inputConstants1.put(0, TypedConstant.create(TypedConstant.create(new Integer(1))));
 		AccessTerm access1 = AccessTerm.create(this.R, this.method1, inputConstants1);
 		AccessTerm access2 = AccessTerm.create(this.S, this.method1);
 		DependentJoinTerm plan1 = DependentJoinTerm.create(access1, access2);
-		
-		NaiveCardinalityEstimator cardinalityEstimator = new NaiveCardinalityEstimator(this.catalog);		
+
+		NaiveCardinalityEstimator cardinalityEstimator = new NaiveCardinalityEstimator(this.catalog);
 		TextBookCostEstimator estimator = new TextBookCostEstimator(null, cardinalityEstimator);
 		Cost cost = estimator.cost(plan1);
-		
-		//TODO add assertions for cost
+
+		Assert.assertEquals(549.5945713886306, (double) cost.getValue(), 0.0001);
 	}
-	
-	@Test public void test7() {
+
+	@Test
+	public void test7() {
 		AccessTerm access1 = AccessTerm.create(this.R, this.method0);
 		AccessTerm access2 = AccessTerm.create(this.S, this.method0);
 		JoinTerm plan1 = JoinTerm.create(access1, access2);
-		
-		NaiveCardinalityEstimator cardinalityEstimator = new NaiveCardinalityEstimator(this.catalog);		
+
+		NaiveCardinalityEstimator cardinalityEstimator = new NaiveCardinalityEstimator(this.catalog);
 		TextBookCostEstimator estimator = new TextBookCostEstimator(null, cardinalityEstimator);
 		Cost cost = estimator.cost(plan1);
-		
-		//TODO add assertions for cost
+
+		Assert.assertEquals(93684.95477555826, (double) cost.getValue(), 0.0001);
 	}
-	
+
 }
