aa8fcca5f51db1e26e6bfad33ac5c376b4155af8
Gabor <Gabor@192.168.18.46>
2017-10-23 14:52:14 +0100

    Summary for simple cost calculations as a unit test

---
+++ cost/test/src/uk/ac/ox/cs/pdq/test/cost/estimators/statistics/estimators/OrderIndependentCostEstimatorTest.java
@@ -0,0 +1,103 @@
+package uk.ac.ox.cs.pdq.test.cost.estimators.statistics.estimators;
+
+import static org.mockito.Mockito.when;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import uk.ac.ox.cs.pdq.algebra.AccessTerm;
+import uk.ac.ox.cs.pdq.algebra.JoinTerm;
+import uk.ac.ox.cs.pdq.cost.estimators.CountNumberOfAccessedRelationsCostEstimator;
+import uk.ac.ox.cs.pdq.cost.estimators.FixedCostPerAccessCostEstimator;
+import uk.ac.ox.cs.pdq.cost.estimators.TotalNumberOfOutputTuplesPerAccessCostEstimator;
+import uk.ac.ox.cs.pdq.cost.statistics.SimpleCatalog;
+import uk.ac.ox.cs.pdq.test.util.PdqTest;
+
+/**
+ * This unit test aims to demonstrate some of the OrderIndependentCostEstimators
+ * and how they can be used from the most simple to the more complicated ones.
+ * <br/>
+ * 
+ * <li>CountNumberOfAccessedRelationsCostEstimator - this will give the number
+ * of AccessTerms in the plan. No input required.</li>
+ * 
+ * <li>FixedCostPerAccessCostEstimator - this gives weight to each access term,
+ * a catalog object have to be provided as input with the cost values for each
+ * access method.</li>
+ * 
+ * <li>TotalNumberOfOutputTuplesPerAccessCostEstimator -The cost of the plan
+ * equals the total number of output tuples per access. Requires column
+ * cardinalities given as a catalog.</li>
+ * 
+ * @author Gabor
+ *
+ */
+public class OrderIndependentCostEstimatorTest extends PdqTest {
+
+	/**
+	 * Creates a dummy plan with 3 access terms. The
+	 * CountNumberOfAccessedRelationsCostEstimator should return 3.0 as cost.
+	 */
+	@Test
+	public void CountNumberOfAccessedRelationsCostEstimatorTest() {
+		// plan
+		AccessTerm at1 = AccessTerm.create(R, method0);
+		AccessTerm at2 = AccessTerm.create(S, method0);
+		AccessTerm at3 = AccessTerm.create(T, method0);
+		JoinTerm jt1 = JoinTerm.create(at1, at2);
+		JoinTerm jt2 = JoinTerm.create(jt1, at3);
+		
+		// cost
+		CountNumberOfAccessedRelationsCostEstimator estimator = new CountNumberOfAccessedRelationsCostEstimator(null);
+		Assert.assertEquals(3.0, estimator.cost(jt2).getCost(), 0.0001);
+	}
+
+	/**
+	 * According to this implementation, the cost of a plan equals the sum of the
+	 * costs of the associated accesses. ( 100 + 110 + 101 = 311 )
+	 */
+	@Test
+	public void FixedCostPerAccessCostEstimatorTest() {
+		// plan
+		AccessTerm at1 = AccessTerm.create(R, method0);
+		AccessTerm at2 = AccessTerm.create(S, method0);
+		AccessTerm at3 = AccessTerm.create(T, method0);
+		JoinTerm jt1 = JoinTerm.create(at1, at2);
+		JoinTerm jt2 = JoinTerm.create(jt1, at3);
+		
+		// cost inputs
+		SimpleCatalog catalog = Mockito.mock(SimpleCatalog.class);
+		when(catalog.getCost(R, method0)).thenReturn(100.0);
+		when(catalog.getCost(S, method0)).thenReturn(110.0);
+		when(catalog.getCost(T, method0)).thenReturn(101.0);
+		
+		// cost calculation
+		FixedCostPerAccessCostEstimator estimator = new FixedCostPerAccessCostEstimator(null, catalog);
+		Assert.assertEquals(311.0, estimator.cost(jt2).getCost(), 0.0001);
+
+	}
+
+	/**
+	 * The cost of the plan equals the total number of output tuples per access .
+	 */
+	@Test
+	public void TotalNumberOfOutputTuplesPerAccessCostEstimatorTest() {
+		// plan 
+		AccessTerm at1 = AccessTerm.create(R, method0);
+		AccessTerm at2 = AccessTerm.create(S, method0);
+		AccessTerm at3 = AccessTerm.create(T, method0);
+		JoinTerm jt1 = JoinTerm.create(at1, at2);
+		JoinTerm jt2 = JoinTerm.create(jt1, at3);
+		
+		// cost inputs
+		SimpleCatalog catalog = Mockito.mock(SimpleCatalog.class);
+		when(catalog.getTotalNumberOfOutputTuplesPerInputTuple(R, method0)).thenReturn(100);
+		when(catalog.getTotalNumberOfOutputTuplesPerInputTuple(S, method0)).thenReturn(110);
+		when(catalog.getTotalNumberOfOutputTuplesPerInputTuple(T, method0)).thenReturn(101);
+
+		// cost calculation
+		TotalNumberOfOutputTuplesPerAccessCostEstimator estimator = new TotalNumberOfOutputTuplesPerAccessCostEstimator(null, catalog);
+		Assert.assertEquals(311.0, estimator.cost(jt2).getCost(), 0.0001);
+	}
+}
