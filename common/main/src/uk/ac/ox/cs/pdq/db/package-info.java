15d05194e11cbd53c9b69b7086bea8a1f2591990
Michael Benedikt <benedikt@linux1.cs.ox.ac.uk>
2018-04-05 14:10:10 +0100

    add documentation

--- common/main/src/uk/ac/ox/cs/pdq/db/package-info.java
+++ common/main/src/uk/ac/ox/cs/pdq/db/package-info.java
@@ -13,6 +13,7 @@ package uk.ac.ox.cs.pdq.db;
 	
 	The constraints that are currently supproted are 
 	-TGDS: dependencies of the form \delta = \forall x  \sigma(\vec{x}) --> \exists y  \tau(\vec{x}, \vec{y}) where \sigma and \tau are conjunctions of atoms.
+TOCOMMENT: THIS NEXT SEEMS OUT OF DATE
 	-Linear guarded dependencies: dependencies of the form \delta = \forall x  \sigma(\vec{x}) --> \exists y  \tau(\vec{x}, \vec{y}) where \sigma is a single atim and \tau is a conjunction of atoms.
 	-EGDs: dependencies of the form \delta = \forall \vec{x} \rho(\vec{x}) --> x_i = x_j where \rho is a conjunction of atoms
 	
