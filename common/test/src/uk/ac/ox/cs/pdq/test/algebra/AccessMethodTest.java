911bae4d97a55436f86890174a7618cd221b209a
marler <marler@clpc173.cs.ox.ac.uk>
2018-04-24 14:08:10 +0100

    Recovered from review branch

--- common/test/src/uk/ac/ox/cs/pdq/test/algebra/AccessMethodTest.java
+++ common/test/src/uk/ac/ox/cs/pdq/test/algebra/AccessMethodTest.java
@@ -1,3 +1,4 @@
+
 package uk.ac.ox.cs.pdq.test.algebra;
 
 import org.junit.Assert;
@@ -5,6 +6,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import uk.ac.ox.cs.pdq.db.AccessMethod;
+import uk.ac.ox.cs.pdq.db.Attribute;
+import uk.ac.ox.cs.pdq.db.Relation;
 import uk.ac.ox.cs.pdq.test.util.PdqTest;
 
 /**
@@ -12,7 +15,7 @@ import uk.ac.ox.cs.pdq.test.util.PdqTest;
  * @author Gabor
  *
  */
-public class AccessMethodTest {
+public class AccessMethodTest extends PdqTest {
 
 	public AccessMethodTest() {
 	}
@@ -29,18 +32,25 @@ public class AccessMethodTest {
 	 */
 	@Test
 	public void testCreation() {
+		Relation relation = new Relation("test",new Attribute[] {a,b,c});
+		AccessMethod am1 = method_0.getMethod(relation);
+// AccessMethods are not cached any more.
+//		Relation relationB = new Relation("test1",new Attribute[] {a,b,c});
 		
-		AccessMethod am1 = AccessMethod.create("test",new Integer[] {0});
-		AccessMethod am2 = AccessMethod.create("test1",new Integer[] {0});
-		AccessMethod am3= AccessMethod.create("test",new Integer[] {0});
+//		AccessMethod am2 = method_0.getMethod(relationB);
+//		AccessMethod am3= method_0.getMethod(relation);
 		
-		if (am1 != am3) { // ATTENTIONAL! it have to be the same reference
-			Assert.fail("AccessMethod cache does not provide same reference");
-		}
-		if (am1 == am2) { // ATTENTIONAL! it have to be the same reference
-			Assert.fail("AccessMethod cache provides same reference when it shouldn't");
-		}
+		// Test that 2 AccessMethods with the same name will be the same
+//		if (am1 != am3) {
+//			Assert.fail("AccessMethod cache does not provide same reference");
+//		}
+//		
+//		// Test that 2 Access methods with different names will not be the same
+//		if (am1 == am2) {
+//			Assert.fail("AccessMethod cache provides same reference when it shouldn't");
+//		}
 		
+		// Test that the name is as we would expect
 		Assert.assertEquals("test",am1.getName());
 	}
 }
