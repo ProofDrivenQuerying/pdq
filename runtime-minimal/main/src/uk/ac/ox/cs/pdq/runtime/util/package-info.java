7e4950e706478dc1cd152ec501733470379cf7a8
Gabor <gabor.gyorkei@gmail.com>
2018-05-15 14:55:51 +0100

    Deleted datasources-old and runtime-minimal.

--- runtime-minimal/main/src/uk/ac/ox/cs/pdq/runtime/util/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Contains utilities for the runtime project.
- * 
- * These typically include post-processing class to be used along with executor,
- * for tuple counting, tuple printing, or other reporting purposes. 
- * 
- */
-package uk.ac.ox.cs.pdq.runtime.util;
