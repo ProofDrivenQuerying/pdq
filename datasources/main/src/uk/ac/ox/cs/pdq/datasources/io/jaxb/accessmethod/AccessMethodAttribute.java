d706c791676fb7c83d96f87e10d37e1e312a04b1
marler <marler@clpc173.cs.ox.ac.uk>
2018-05-16 10:38:42 +0100

    AccessMethod classes

---
+++ datasources/main/src/uk/ac/ox/cs/pdq/datasources/io/jaxb/accessmethod/AccessMethodAttribute.java
@@ -0,0 +1,76 @@
+package uk.ac.ox.cs.pdq.datasources.io.jaxb.accessmethod;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * @author Mark Ridler
+ *
+ */
+// AccessMethodAttribute is the XML element corresponding to the <attribute> tag
+@XmlType (propOrder= {"name", "type", "input", "relationAttribute"})
+public class AccessMethodAttribute {
+	
+	private String name;
+	private String type;
+	private String input;
+	private String output;
+	private String value;
+	private String param;
+	private String relationAttribute;
+	
+	@XmlAttribute
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@XmlAttribute
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	@XmlAttribute
+	public String getInput() {
+		return input;
+	}
+	public void setInput(String input) {
+		this.input = input;
+	}
+	@XmlAttribute
+	public String getOutput() {
+		return output;
+	}
+	public void setOutput(String output) {
+		this.output = output;
+	}
+	@XmlAttribute
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	@XmlAttribute
+	public String getParam() {
+		return param;
+	}
+	public void setParam(String param) {
+		this.param = param;
+	}
+	@XmlAttribute (name = "relation-attribute")
+	public String getRelationAttribute() {
+		return relationAttribute;
+	}
+	public void setRelationAttribute(String relationAttribute) {
+		this.relationAttribute = relationAttribute;
+	}
+	public String toString()
+	{
+		return "Attribute";
+	}
+	
+}
