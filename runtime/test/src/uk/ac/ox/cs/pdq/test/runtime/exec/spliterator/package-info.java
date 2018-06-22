3d4ca918140fbb2d554bf5c48a977e995a420b21
marler <marler@clpc173.cs.ox.ac.uk>
2018-06-22 11:38:07 +0100

    Package-info for runtime/test

---
+++ runtime/test/src/uk/ac/ox/cs/pdq/test/runtime/exec/spliterator/package-info.java
@@ -0,0 +1,28 @@
+package uk.ac.ox.cs.pdq.test.runtime.exec.spliterator;
+
+/**
+	@author Mark Ridler
+	
+	This package contains unit tests:
+	
+	- AccessTest.java
+		* The following are integration tests: Access plans are constructed & executed.
+	- CartesianProductTest.java
+ 		* The following are integration tests: CartesianProduct plans are constructed &
+ 		* executed.
+	- DependentJoinTest.java
+ 		* this test case is very slow, so disabling it for now. Last time I run it one failed, the rest run successfully.
+	- NestedLoopJoinTest.java
+		* The following are integration tests: NestedLoop joins are constructed &
+ 		* executed.
+	- ProjectionTest.java
+		* The following are integration tests: Projections are constructed & executed.
+	- SelectionTest.java
+		* The following are integration tests: Selections are constructed & executed.
+	- SymmetricMemoryHashJoinTest.java
+		* The following are integration tests: SymmetricMemoryHash joins are constructed &
+ 		* executed.
+	- TPCHelper.java
+		* Creates a load of attributes, relations and access methods.
+	
+**/
