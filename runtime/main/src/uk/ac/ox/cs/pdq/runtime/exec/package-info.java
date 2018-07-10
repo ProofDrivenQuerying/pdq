3ae3c114ebd3d86db8719e0beed6b9696f692aa1
marler <marler@clpc173.cs.ox.ac.uk>
2018-07-10 11:30:16 +0100

    Updated package-info.java

--- runtime/main/src/uk/ac/ox/cs/pdq/runtime/exec/package-info.java
+++ runtime/main/src/uk/ac/ox/cs/pdq/runtime/exec/package-info.java
@@ -11,4 +11,17 @@ package uk.ac.ox.cs.pdq.runtime.exec;
 	 	* the executablePlan to make sure it will use the same parameters/access
 	 	* repository to decorate the children as well.
 
+	This also contains the following packages:
+	
+	- runtime.exec.spliterator
+		* Algorithm:
+		* - advance to the next right tuple; if the right child is exhausted:
+		* 		- update the cache of matching tuples from the right child
+		* 		- set the flag to use the cache of accessed right tuples
+		* 		- advance to the next left tuple and reset the right child spliterator
+		* - Add the right tuple to the cache of accessed right tuples (unless that cache is already in use) 
+		* - Prepend the current leftTuple to the current rightTuple
+		* - Test the join condition on the joined tuple
+		* 		- recursively call this method if the condition is not satisfied
+		* - Pass the joined tuple to the given action & return true
 **/
