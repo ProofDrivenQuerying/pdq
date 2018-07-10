2b2d67f33230ead39b65fffa6ce82d2658cf7cd2
marler <marler@clpc173.cs.ox.ac.uk>
2018-07-10 10:42:23 +0100

    Updated package-info.java

--- datasources/main/src/uk/ac/ox/cs/pdq/datasources/package-info.java
+++ datasources/main/src/uk/ac/ox/cs/pdq/datasources/package-info.java
@@ -31,5 +31,45 @@ package uk.ac.ox.cs.pdq.datasources;
  		* beginning of the underlying Iterable at any time.
 	- TranslatingIterator.java
  		* An iterator consuming another iterator.
+ 	
+ 	The other packages consist of:
+ 	
+ 	- datasources.accessrepository
+ 		* It maintains a repository executable access methods. The access methods are defined in xml descriptors in a folder.
+ 	- datasources.builder 
+ 		* Builder are typically use to instantiate objects that are too complex to
+ 		* initialise with a single constructor calls, e.g. if many fields are
+ 		* mandatory and many consistency checks are required.
+ 	- datasources.io.jaxb
+ 		* Represents all executable access methods as xml files. Its main
+ 		* purpose is to have all necessary parameters to be able to create a
+ 		* DbAccessMethod, RestAccessMethod or InMemoryAccessMethod.
+ 	- datasources.io.jaxb.adapted
+ 		* This is a JAXB Xml parsing file complete with annotations. It breaks things down
+		* into  sources, name, relations, description, dependencies and ars;
+	- datasources.io.xml
+		* Reads experiment sample elements from XML.
+		* Mostly unused, outdated by jaxb IO implementation.
+		* Gather all the qualified names appearing in a schema file 
+ 		* query file or plan file
+	- datasources.memory
+	 	* Represents an in-memory version of an executable access method from which it is
+ 		* derived.
+ 	- datasources.services
+ 		* A collection of classes for implementing RESTExecutableAccessMethod by parsing xml files
+ 		* and utilising UsagePolicy and RESTRequest/ResponseEvent.
+ 	- datasources.services.policies
+ 		* A collection of UsagePolicies
+ 	- datasources.services.service
+ 		* A collection of files for parsing the xml Service definition
+ 	- datasources.services.servicegroup
+		* A collection of files for parsing the xml Service Group definition
+	- datasources.sql
+		* This package contains classes that build DBMS wrappers, i.e., methods that access data from different DBMSs.
+		* The current implementation supports accessing data from MySQL and Postgres databases.
+	- datasources.utility
+		* A collection of utility classes for BooleanResult, FormulaEquivalance, SchemaConverter etc
+
+
 
 **/
