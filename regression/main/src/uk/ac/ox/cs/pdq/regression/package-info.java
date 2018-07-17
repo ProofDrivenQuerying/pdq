fb819cd0efa8be03c0486de08aea3be38e0d2ca3
marler <marler@clpc173.cs.ox.ac.uk>
2018-07-17 10:19:19 +0100

    Updated package-info.java

--- regression/main/src/uk/ac/ox/cs/pdq/regression/package-info.java
+++ regression/main/src/uk/ac/ox/cs/pdq/regression/package-info.java
@@ -4,19 +4,23 @@ package uk.ac.ox.cs.pdq.regression;
 	@author Mark Ridler and Efthymia Tsamoura
 	@author gabor
 	
-	This package contains:
+	This package is the entry point for running regression tests.
+	There are different types of regression tests:
+	-planner tests: sanity checks on different planners. These tests can be found in the planner package.
+	-runtime tests: sanity checks on the runtime. These tests can be found in the planner package.
 	
-	- PDQ.java
-		* The entry point for the regression package.
-	- RegressionParameters.java
-		* Hold the initialConfig of an execution.
-<<<<<<< HEAD
-	- RegressionTest.java
-		* Runs regression tests.
-	- RegressionTestException.java
- 		* Exception that occurred during a regression test.
+	The regression tests require input files:
+	- case.properties: parameters specific to a single execution
+	- schema.xml: the input schema
+	- query.xml: the input query
+	- [optional] expected-plan.xml: When exists the generated plan will be compared against it, otherwise PDQ in planner mode creates it.
+
+	Prominent files include:
+	
+	- PDQ.java which is the entry point for the regression package.
+	- RegressionTest.java which runs regression tests.
  	
- 	This also contains the following packages:
+ 	This also contains the following sub packages:
  	
  	- regression.acceptance
 		* This package runs regression tests with a theme of acceptance criteria.
@@ -33,17 +37,5 @@ package uk.ac.ox.cs.pdq.regression;
 	 	* resulting plans. An exception is thrown when the plans are different.
  	- regression.runtime
 		* This package is commented out pending resolution of the runtime changes.
-=======
->>>>>>> branch 'master' of https://github.com/michaelbenedikt/pdq.git
 
-	This package is the entry point for running regression tests.
-	There are different types of regression tests:
-	-planner tests: sanity checks on different planners. These tests can be found in the planner package.
-	-runtime tests: sanity checks on the runtime. These tests can be found in the planner package.
-	
-	The regression tests require input files:
-	- case.properties: parameters specific to a single execution
-	- schema.xml: the input schema
-	- query.xml: the input query
-	- [optional] expected-plan.xml: When exists the generated plan will be compared against it, otherwise PDQ in planner mode creates it.
-**/
+*/
