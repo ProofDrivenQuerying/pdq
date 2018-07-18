cfca5c9fe234faa04634543cae66d8fba99d18a9
Michael Benedikt <benedikt@linux2.cs.ox.ac.uk>
2018-07-18 12:15:55 +0100

    fix to packageinfo

--- datasources/main/src/uk/ac/ox/cs/pdq/datasources/package-info.java
+++ datasources/main/src/uk/ac/ox/cs/pdq/datasources/package-info.java
@@ -11,45 +11,45 @@ The package includes:
 --  methods for calling the external sources, which are used as a base step
 in the plan language
 -- tools for creating these objects from XML spefications of services
---  tools for reading these datasource objects from external repositories (currently,
+-- tools for reading these datasource objects from external repositories (currently,
 from SQL-based databases).
 
-Two XML-based specifications are maintained, the current one, which
-is fully operatioinal; a legacy one, which can be parsed but
+Two XML-based specifications of services are maintained. The important
+one is the current format, which
+is fully operatioinal. There is also a legacy one, which can be parsed but
 the resulting parsed files cannot be used; it is useful only for creating converters
 to the current format.
 	
-	Subpackages include 
+Subpackages include:
 
- --- the access repository, which represents a directory of services
+-- the access repository, which represents a directory of services
 
---schema builder, which allows one to read in external schemas (currently from a DBMS)
+-- schema builder, which allows one to read in external schemas (currently from a DBMS)
 
--- io.jaxb, for reading specifications in the current format;
+-- io.jaxb, for reading specifications in the current format
 
 -- io.xml for reading specifications in the old format
 
--- legacy services, givng the java object representation of the legacy REST metadata specifications 
+-- legacy services, giving the java object representation of the legacy REST metadata specifications 
 
 -- services, giving the java object representation of the current REST services -- metadata
-nad access
+and methods to access the data
 
 -- SQL, giving the java object representations of SQL-based datasources
 
--- memory, giving the java object representation of acces to main memory data
+-- memory, giving the java object representation of access to main memory data
 
 -- utility classes
   	
-	 The main top-level files in the package 
-	is  ExecutableAccessMethod.java
- 		* This class extends the functionality of an AccessMethodDescriptor (an
- 		* abstract descriptioin of the service), extending it by including the
- 		* ability to execute the access. An ExecutableAccessMethod is described
- 		* as a function with some input attributes, while an AccessMethodDescriptor is described
- 		* as giving access to a relation on some input positions. It is thus
- 		* necessary to map between attributes of the ExecutableAccessMethod
- 		* and positions of the corresponding relation, and this class includes
- 		* that mpaping.
+ The main top-level file in the package is  ExecutableAccessMethod.java
+ This class extends the functionality of an AccessMethodDescriptor, which is an
+ abstract description of an access method. The extension includes a method for
+executing the access method. An ExecutableAccessMethod is described
+ as a function with some input attributes, while an AccessMethodDescriptor is described
+ as giving access to a relation on some input positions. It is thus
+ necessary to map between attributes of the ExecutableAccessMethod
+and positions of the corresponding relation, and this class includes
+the necessary mapping information.
 
 
 
