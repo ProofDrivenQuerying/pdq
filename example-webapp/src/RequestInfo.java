eb1433beeb9accd4e7aad449bca8098b59cb5ba0
marler <marler@clpc173.cs.ox.ac.uk>
2018-07-18 10:24:59 +0100

    Updated text

--- example-webapp/src/RequestInfo.java
+++ example-webapp/src/RequestInfo.java
@@ -16,20 +16,20 @@ public class RequestInfo extends HttpServlet {
 	/**
 	 * @author Mark Ridler
 	 * 
-	 * This webapp implements a simple retrusn of a table from a Postgres
+	 * This webapp implements a simple return of a table from a Postgres
 	 * database. It returns in XML format to be compatible with the REST
 	 * services on the client side.
 	 * 
 	 * This is best deployed as a Dynamic Web Project in Eclipse. This is
 	 * not installed by default so you will have to upgrade. Alongside
 	 * this goes the Apache Tomcat server which again can be integrated with
-	 * Eclipse on the Servers tab. Then rightmouse click on the tomcat and
+	 * Eclipse on the Servers tab. Then right mouse click on the tomcat and
 	 * select "Add and Remove" to add the dynamic web project. After this has
 	 * been done you will be able to automatically publish to the tomcat
 	 * whenever the source file has changed.
 	 * 
 	 * The one thing that we haven't been able to get working is the
-	 * integration with git. At the moment a sepaarate git project has the
+	 * integration with git. At the moment a separate git project has the
 	 * source files which must be copied into the dynamic web project and
 	 * vice versa
 	 * 
