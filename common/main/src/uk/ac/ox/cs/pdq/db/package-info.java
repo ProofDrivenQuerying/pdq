d3954abe130a560ac8fd4ffaacf5e3cb220c7c02
marler <marler@clpc173.cs.ox.ac.uk>
2018-07-20 10:41:56 +0100

    Updated package-info.java

--- common/main/src/uk/ac/ox/cs/pdq/db/package-info.java
+++ common/main/src/uk/ac/ox/cs/pdq/db/package-info.java
@@ -1,20 +1,22 @@
 package uk.ac.ox.cs.pdq.db;
 
 /**
-	@author Efthymia Tsamoura
+	@author Mark Ridler
 
-	This package defines several relational database-related objects including
-	-relations
-	-attributes
-	-keys and foreign keys
-	-access methods (an access method defines the positions of a relation's attributes whose values are required to access the relation)
-	-views
-	-and constraints. 
+	This db sub-package defines several relational database-related objects including relations, attributes, keys and foreign keys,
+	access method descriptors and views.
 	
-	The constraints that are currently supproted are 
-	-TGDS: dependencies of the form \delta = \forall x  \sigma(\vec{x}) --> \exists y  \tau(\vec{x}, \vec{y}) where \sigma and \tau are conjunctions of atoms.
-TOCOMMENT: THIS NEXT SEEMS OUT OF DATE
-	-Linear guarded dependencies: dependencies of the form \delta = \forall x  \sigma(\vec{x}) --> \exists y  \tau(\vec{x}, \vec{y}) where \sigma is a single atim and \tau is a conjunction of atoms.
-	-EGDs: dependencies of the form \delta = \forall \vec{x} \rho(\vec{x}) --> x_i = x_j where \rho is a conjunction of atoms
+	The sub-package contains:
 	
+	-- AccessMethodDescriptor, which represents a descriptor for an access method rather than the access method itself
+	-- Attribute, which represents a relation's attribute
+	-- Cache, which creates and maintains a cache of each object type in this package
+	-- ForeignKey, which represents a database foreign key.
+	-- Instance, which models a relational database instance. An Instance is a set of Atoms.
+	-- Match, which represents a formula or query that will be grounded using an homomorphism
+	-- PrimaryKey, which represents a database primary key.
+	-- Reference, which represents a reference between a column in the local table and a column in another table.
+	-- Relation, which represents the schema of a relation.
+	-- Schema, which represents a database schema.
+	-- View, which represents a database view.
 **/
