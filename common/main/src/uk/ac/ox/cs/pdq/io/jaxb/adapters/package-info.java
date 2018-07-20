a3183f60008eaf6ac0577172e4d83973f12f68df
marler <marler@clpc173.cs.ox.ac.uk>
2018-07-20 15:41:53 +0100

    Updated package-info.java

---
+++ common/main/src/uk/ac/ox/cs/pdq/io/jaxb/adapters/package-info.java
@@ -0,0 +1,11 @@
+package uk.ac.ox.cs.pdq.io.jaxb.adapters;
+
+/**
+ * 
+ * @author Mark Ridler
+ * 
+ * The adapter package consists of a collection of classes in the same format, namely that they
+ * subclass the XmlAdapter class. The main function creates an object of the corresponding Adapted
+ * class.
+ * 
+ */
