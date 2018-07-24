6ae62f3fdd1f0d11540c608401f27a9fc9fc5b31
marler <marler@clpc173.cs.ox.ac.uk>
2018-07-24 14:13:13 +0100

    More to add to previous commit

--- datasources/test/src/uk/ac/ox/cs/pdq/test/datasources/services/ServiceXmlTest.java
+++ datasources/test/src/uk/ac/ox/cs/pdq/test/datasources/services/ServiceXmlTest.java
@@ -168,14 +168,41 @@ public class ServiceXmlTest {
 		}
 	}
 
-	// pdq webapp
+	// pdq webapp nation input
 	@Test
 	public void test6() {
 		
 		try
 		{
 			File schemaFile1 = new File("test" + File.separator + "src" + File.separator + "uk" + File.separator + "ac" + File.separator + "ox" + File.separator + "cs" + File.separator + "pdq" + File.separator + "test" + File.separator + "datasources" + File.separator + "services" + File.separator + "service-groups.xml");
-			File schemaFile2 = new File("test" + File.separator + "src" + File.separator + "uk" + File.separator + "ac" + File.separator + "ox" + File.separator + "cs" + File.separator + "pdq" + File.separator + "test" + File.separator + "datasources" + File.separator + "services" + File.separator + "pdq-webapp.xml");
+			File schemaFile2 = new File("test" + File.separator + "src" + File.separator + "uk" + File.separator + "ac" + File.separator + "ox" + File.separator + "cs" + File.separator + "pdq" + File.separator + "test" + File.separator + "datasources" + File.separator + "services" + File.separator + "pdq-webapp-nationInput.xml");
+			ServiceGroup sgr = ServiceManager.importServiceGroups(schemaFile1);
+			Service sr = ServiceManager.importAccessMethod(schemaFile2);
+			System.out.println(sr.toString());
+			for(int i = 0; i < sr.getAccessMethod().length; i++)
+			{
+				RESTExecutableAccessMethodSpecification am = sr.getAccessMethod()[i];
+				TupleType tupleType = TupleType.DefaultFactory.createFromTyped();
+				Tuple input = tupleType.createTuple();
+				RESTAccessMethodGenerator ream = new RESTAccessMethodGenerator(sgr, sr, am);
+				Table t = ream.getRestAccessMethod().accessTable(input);
+				System.out.println(t);
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+	// pdq webapp country free
+	@Test
+	public void test7() {
+		
+		try
+		{
+			File schemaFile1 = new File("test" + File.separator + "src" + File.separator + "uk" + File.separator + "ac" + File.separator + "ox" + File.separator + "cs" + File.separator + "pdq" + File.separator + "test" + File.separator + "datasources" + File.separator + "services" + File.separator + "service-groups.xml");
+			File schemaFile2 = new File("test" + File.separator + "src" + File.separator + "uk" + File.separator + "ac" + File.separator + "ox" + File.separator + "cs" + File.separator + "pdq" + File.separator + "test" + File.separator + "datasources" + File.separator + "services" + File.separator + "pdq-webapp-countryFree.xml");
 			ServiceGroup sgr = ServiceManager.importServiceGroups(schemaFile1);
 			Service sr = ServiceManager.importAccessMethod(schemaFile2);
 			System.out.println(sr.toString());
