30ecce1ade2f757d06671a470c17e00727f928a4
Gabor <gabor.gyorkei@gmail.com>
2018-09-27 16:28:05 +0100

    Simplified version

--- planner/main/src/uk/ac/ox/cs/pdq/planner/dag/explorer/DAGOptimizedNew.java
+++ planner/main/src/uk/ac/ox/cs/pdq/planner/dag/explorer/DAGOptimizedNew.java
@@ -1,7 +1,6 @@
 package uk.ac.ox.cs.pdq.planner.dag.explorer;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -10,13 +9,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.collections4.CollectionUtils;
@@ -33,11 +28,11 @@ import uk.ac.ox.cs.pdq.fol.Dependency;
 import uk.ac.ox.cs.pdq.planner.PlannerException;
 import uk.ac.ox.cs.pdq.planner.PlannerParameters;
 import uk.ac.ox.cs.pdq.planner.accessibleschema.AccessibleSchema;
+import uk.ac.ox.cs.pdq.planner.dag.ConfigurationUtility;
 import uk.ac.ox.cs.pdq.planner.dag.DAGChaseConfiguration;
 import uk.ac.ox.cs.pdq.planner.dag.equivalence.DAGEquivalenceClasses;
 import uk.ac.ox.cs.pdq.planner.dag.equivalence.SynchronizedEquivalenceClasses;
 import uk.ac.ox.cs.pdq.planner.dag.explorer.filters.Filter;
-import uk.ac.ox.cs.pdq.planner.dag.explorer.parallel.ConfigurationSpaceExplorationThread;
 import uk.ac.ox.cs.pdq.planner.dag.explorer.parallel.CreateBinaryConfigurationsThread;
 import uk.ac.ox.cs.pdq.planner.dag.explorer.parallel.ExplorationResults;
 import uk.ac.ox.cs.pdq.planner.dag.explorer.parallel.MapOfPairsOfConfigurationsToTheEquivalentBinaryConfiguration;
@@ -49,14 +44,14 @@ import uk.ac.ox.cs.pdq.planner.dominance.SuccessDominance;
 import uk.ac.ox.cs.pdq.planner.dominance.SuccessDominanceFactory;
 import uk.ac.ox.cs.pdq.reasoning.chase.Chaser;
 import uk.ac.ox.cs.pdq.util.LimitReachedException;
-import uk.ac.ox.cs.pdq.util.LimitReachedException.Reasons;
 
 /**
  * An explorer for plans using ideas from dynamic programming. It performs
  * parallel chasing and (success-)dominance, equivalence and success checks in
  * parallel
  * 
- * @author Efthymia Tsamoura
+ * Similar to DAG Optimized, but the code is simplified.
+ * @author Gabor
  *
  */
 public class DAGOptimizedNew extends DAGExplorer {
@@ -84,38 +79,10 @@ public class DAGOptimizedNew extends DAGExplorer {
 	/**
 	 * Instantiates a new DAG optimized.
 	 *
-	 * @param eventBus
-	 *            the event bus
-	 * @param collectStats
-	 *            the collect stats
-	 * @param parameters
-	 *            the parameters
-	 * @param query
-	 *            The input user query
-	 * @param accessibleQuery
-	 *            The accessible counterpart of the user query
-	 * @param schema
-	 *            The input schema
-	 * @param accessibleSchema
-	 *            The accessible counterpart of the input schema
-	 * @param chaser
-	 *            Runs the chase algorithm
-	 * @param detector
-	 *            Detects homomorphisms during chasing
-	 * @param costEstimator
-	 *            Estimates the cost of a plan
 	 * @param filter
 	 *            Filters out configurations at the end of each iteration
-	 * @param binaryConfigurationCreationThreads
-	 *            Performs parallel chasing
-	 * @param configurationSpaceExplorationThreads
-	 *            Iterates over all newly created configurations in parallel and
-	 *            returns the best configuration
 	 * @param maxDepth
 	 *            The maximum depth to explore
-	 * @throws PlannerException
-	 *             the planner exception
-	 * @throws SQLException
 	 */
 	public DAGOptimizedNew(EventBus eventBus, PlannerParameters parameters, ConjunctiveQuery query,
 			ConjunctiveQuery accessibleQuery, AccessibleSchema accessibleSchema, Chaser chaser,
@@ -170,8 +137,7 @@ public class DAGOptimizedNew extends DAGExplorer {
 		} else if (this.depth > 1) {
 			this.checkLimitReached();
 			// Perform parallel chasing
-			Collection<DAGChaseConfiguration> newlyCreatedConfigurations = this.createBinaryConfigurations(
-					this.depth, this.leftSideConfigurations, this.equivalenceClasses.getConfigurations(),
+			Collection<DAGChaseConfiguration> newlyCreatedConfigurations = this.createBinaryConfigurations(this.leftSideConfigurations, this.equivalenceClasses.getConfigurations(),
 					this.accessibleSchema.getInferredAccessibilityAxioms(), this.bestConfiguration,
 					this.equivalenceClasses, true,
 					Double.valueOf((this.maxElapsedTime - (this.elapsedTime / 1e6))).longValue(),
@@ -196,12 +162,12 @@ public class DAGOptimizedNew extends DAGExplorer {
 				return;
 			}
 			// Update the best configuration
-			List<DAGChaseConfiguration> nonDominatedConfigurations = explorationResults.getNonDominatedConfigurations();
 			DAGChaseConfiguration bestConfiguration = explorationResults.getBest();
 			if (bestConfiguration != null) {
 				this.setBestPlan(bestConfiguration);
 			}
 
+			List<DAGChaseConfiguration> nonDominatedConfigurations = explorationResults.getNonDominatedConfigurations();
 			if (nonDominatedConfigurations.isEmpty()) {
 				this.forcedTermination = true;
 				return;
@@ -222,13 +188,10 @@ public class DAGOptimizedNew extends DAGExplorer {
 		this.depth++;
 	}
 
-	private Collection<DAGChaseConfiguration> createBinaryConfigurations(int depth2,
-			Queue<DAGChaseConfiguration> leftSideConfigurations2, Collection<DAGChaseConfiguration> rightSideConfigurations,
+	private Collection<DAGChaseConfiguration> createBinaryConfigurations(Queue<DAGChaseConfiguration> leftSideConfigurations, Collection<DAGChaseConfiguration> rightSideConfigurations,
 			Dependency[] inferredAccessibilityAxioms, DAGChaseConfiguration bestConfiguration,
 			DAGEquivalenceClasses equivalenceClasses2, boolean b, long longValue, TimeUnit milliseconds) {
 
-		// Create a pool of threads to run in parallel
-		ExecutorService executorService = Executors.newFixedThreadPool(1);
 		// Map of representatives. For each configuration c = BinConfiguration(c_1,c_2)
 		// we create a map from the
 		// equivalence classes of c and c' to c''. This map helps us reducing the
@@ -247,39 +210,21 @@ public class DAGOptimizedNew extends DAGExplorer {
 			Queue<DAGChaseConfiguration> leftInput = new ConcurrentLinkedQueue<>();
 			leftInput.addAll(leftSideConfigurations);
 			Collection<DAGChaseConfiguration> rightInput = rightSideConfigurations;
-			List<CreateBinaryConfigurationsThread> threads = new ArrayList<>();
 			do {
-				for (int j = 0; j < 1; ++j) {
-					// Create the threads that will create new binary configurations using the input
-					// left, right collections
-					threads.add(new CreateBinaryConfigurationsThread(depth, leftInput, rightInput,
+				CreateBinaryConfigurationsThread thread = new CreateBinaryConfigurationsThread(depth, leftInput, rightInput,
 							// query,
 							inferredAccessibilityAxioms, this.chaser, this.connection,
 							this.costEstimator, successDominance,
 							bestConfiguration, Arrays.asList(new Validator[] {this.validator}), equivalenceClasses, representatives,
-							output));
-				}
-				List<Future<Boolean>> results = executorService.invokeAll(threads, 1, TimeUnit.MINUTES);
-				try {
-					for (Future<Boolean> result : results) {
-						result.get();
-					}
-				} catch (java.util.concurrent.CancellationException e) {
-					executorService.shutdownNow();
-					return null;
-				}
-				executorService.shutdown();
-
-				for (CreateBinaryConfigurationsThread t : threads) {
-					if (t.getOutputs().size() > 0)
-						output2.putAll(t.getOutputs());
-				}
+							output);
+				thread.call();				
+				if (thread.getOutputs().size() > 0)
+					output2.putAll(thread.getOutputs());
 				// If twoWay = TRUE create also configurations BinaryConfiguration(R,L), where R
 				// and L belong to the right and left
 				// input collections, respectively.
 				boolean twoWay = false;
 				if (twoWay) {
-					executorService = Executors.newFixedThreadPool(1);
 					leftInput = new ConcurrentLinkedQueue<>(rightSideConfigurations);
 					rightInput = copy;
 					twoWay = false;
@@ -289,12 +234,7 @@ public class DAGOptimizedNew extends DAGExplorer {
 				}
 			} while (true);
 			return output2.values();
-
-		} catch (InterruptedException | ExecutionException e) {
-			executorService.shutdownNow();
-			return null;
 		} catch (Throwable e) {
-			executorService.shutdownNow();
 			e.printStackTrace();
 			return null;
 		}
@@ -303,65 +243,72 @@ public class DAGOptimizedNew extends DAGExplorer {
 			DAGEquivalenceClasses equivalenceClasses, DAGChaseConfiguration bestConfiguration, long timeout,
 			TimeUnit unit) throws PlannerException, LimitReachedException {
 
-		long start = System.currentTimeMillis();
-
-		// Create a pool of threads to run in parallel
-		ExecutorService executorService = Executors.newFixedThreadPool(1);
 		try {
 
 			// The output non-dominated configurations
 			Set<DAGChaseConfiguration> output = Collections
 					.newSetFromMap(new ConcurrentHashMap<DAGChaseConfiguration, Boolean>());
-			// //The output non-dominated and successful (and not closed) configurations
-			// Set<DAGChaseConfiguration> successfulConfigurations =
-			// Collections.newSetFromMap(new ConcurrentHashMap<DAGChaseConfiguration,
-			// Boolean>());
-			List<Callable<DAGChaseConfiguration>> threads = new ArrayList<>();
-			for (int j = 0; j < 1; ++j) {
-				// Create the threads that will iterate over the input configurations to find
-				// the non-dominated ones,
-				// along with the minimum-cost successful and closed one
-				threads.add(new ConfigurationSpaceExplorationThread(query, input, equivalenceClasses, bestConfiguration,
-						successDominance, dominance, output
-				// ,
-				// successfulConfigurations
-				));
-			}
 
-			List<Future<DAGChaseConfiguration>> results = executorService.invokeAll(threads, timeout, unit);
+			DAGChaseConfiguration result = findBestAndUpdateEquivalences(input,bestConfiguration,output);
 			DAGChaseConfiguration configuration = bestConfiguration;
-			try {
-				for (Future<DAGChaseConfiguration> result : results) {
-					DAGChaseConfiguration r = result.get();
-					if (r == null)
-						continue;
-					if (r.getCost() == null) {
-						continue;
-					}
-					if (configuration == null || (r != null && configuration.getCost().greaterThan(r.getCost()))) {
-						configuration = r;
-					}
-				}
-
-				if (equivalenceClasses instanceof SynchronizedEquivalenceClasses) {
-					((SynchronizedEquivalenceClasses) equivalenceClasses)
-							.wakeupSleep(configuration != null ? configuration.getCost() : null);
-				}
-			} catch (java.util.concurrent.CancellationException e) {
-				executorService.shutdownNow();
-				if (timeout <= (System.currentTimeMillis() - start)) {
-					throw new LimitReachedException(Reasons.TIMEOUT);
-				}
-				return null;
+			if (configuration == null || (result != null && configuration.getCost().greaterThan(result.getCost()))) {
+				configuration = result;
 			}
 
-			executorService.shutdown();
+			if (equivalenceClasses instanceof SynchronizedEquivalenceClasses) {
+				((SynchronizedEquivalenceClasses) equivalenceClasses)
+						.wakeupSleep(configuration != null ? configuration.getCost() : null);
+			}
 			return new ExplorationResults(Lists.newArrayList(output), configuration);
 		} catch (InterruptedException | ExecutionException e) {
-			executorService.shutdownNow();
+			handleExceptions(e);
+			return null;
+		} catch (Exception e) {
+			e.printStackTrace();
 			handleExceptions(e);
 			return null;
 		}
+	}
+	
+	private DAGChaseConfiguration findBestAndUpdateEquivalences(Queue<DAGChaseConfiguration>  input,DAGChaseConfiguration bestConfiguration,Set<DAGChaseConfiguration> output) throws Exception {
+		DAGChaseConfiguration configuration;
+		//Poll the next configuration
+		while((configuration = input.poll()) != null) {			
+			// This dominance related stuff needs to be checked, unit tested and then added again.
+			//If the configuration is not dominated
+			DAGChaseConfiguration dominator = this.equivalenceClasses.dominate(this.dominance, configuration);
+			if (dominator != null
+					//ExplorerUtils.isDominated(this.dag, binConfig) != null || ExplorerUtils.isDominated(binConfigs, binConfig) != null
+					) {
+			} else {
+				//Assess its potential
+				if (ConfigurationUtility.getPotential(configuration, bestConfiguration == null ? null : bestConfiguration.getPlan(), bestConfiguration== null ? null : bestConfiguration.getCost(), this.successDominance)) {
+					//Find the configurations dominated by the current one and remove them
+					Collection<DAGChaseConfiguration> dominated = this.equivalenceClasses.dominatedBy(this.dominance, configuration);
+					if(!dominated.isEmpty()) {
+						output.removeAll(dominated);
+						this.equivalenceClasses.removeAll(dominated);
+					}
+					//Update the best configuration
+					if (configuration.isClosed() && configuration.isSuccessful(this.query) == true) {
+						if (bestConfiguration != null && bestConfiguration.getCost() == null && configuration!=null && configuration.getCost() != null) {
+							// in case the old best had no cost the new one is better, no matter what the cost value is.
+							bestConfiguration = configuration;
+						}
+						if (bestConfiguration == null
+								|| (bestConfiguration != null && configuration != null
+								&& bestConfiguration.getCost().greaterThan(configuration.getCost()))) {
+							bestConfiguration = configuration;
+						}
+					}
+					else {
+						this.equivalenceClasses.addEntry(configuration);
+						output.add(configuration);
+					}
+				}
+			}
+		}
+		return bestConfiguration;
 	}
 
 	/**
