b1361e2e19ebc36792a52d9588ae5d587b5dde32
marler <marler@clpc463.cs.ox.ac.uk>
2018-10-09 16:06:10 +0100

    Implements export

--- demo/main/src/uk/ac/ox/cs/pdq/ui/io/ObservableSchemaWriter.java
+++ demo/main/src/uk/ac/ox/cs/pdq/ui/io/ObservableSchemaWriter.java
@@ -1,17 +1,30 @@
 package uk.ac.ox.cs.pdq.ui.io;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
 import org.apache.log4j.Logger;
 
+import com.google.common.base.Preconditions;
+
 import uk.ac.ox.cs.pdq.datasources.legacy.io.xml.AbstractXMLWriter;
 import uk.ac.ox.cs.pdq.datasources.legacy.io.xml.QNames;
+import uk.ac.ox.cs.pdq.datasources.services.service.Service;
+import uk.ac.ox.cs.pdq.datasources.services.servicegroup.ServiceGroup;
 import uk.ac.ox.cs.pdq.io.jaxb.IOManager;
 //import uk.ac.ox.cs.pdq.io.xml.SchemaWriter;
 import uk.ac.ox.cs.pdq.ui.model.ObservableSchema;
@@ -50,6 +63,7 @@ public class ObservableSchemaWriter extends AbstractXMLWriter<ObservableSchema> 
 			PrintStream out = new PrintStream(file);
 			out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 			this.writeSchema(file, o);
+			out.close();
 		}
 		catch(FileNotFoundException e)
 		{
@@ -66,9 +80,53 @@ public class ObservableSchemaWriter extends AbstractXMLWriter<ObservableSchema> 
 		try
 		{
 			IOManager.exportSchemaToXml(s.getSchema(), file);
+			String path1 = file.getAbsolutePath();
+			String path2 = s.getFile().getAbsolutePath();
+			File schemaDir1 = new File(path1 + "d");
+			File schemaDir2 = new File(path2 + "d");
+			schemaDir1.mkdirs();
+			for(File sourceFile : listFiles(schemaDir2, "", ".sr"))
+			{
+				File destFile = new File(path1 + "d/" + sourceFile.getName());
+				copyFile(sourceFile, destFile);
+			}
+			for(File sourceFile : listFiles(schemaDir2, "", ".srg"))
+			{
+				File destFile = new File(path1 + "d/" + sourceFile.getName());
+				copyFile(sourceFile, destFile);
+			}
 		}
-		catch(JAXBException e)
+		catch(JAXBException | IOException e)
 		{
 		}
 	}
+
+private static File[] listFiles(File directory, final String prefix, final String suffix) {
+	Preconditions.checkArgument(directory.isDirectory(), "Invalid internal schema directory " + directory.getAbsolutePath());
+	return directory.listFiles(new FilenameFilter() {
+		@Override
+		public boolean accept(File dir, String name) {
+			return name.startsWith(prefix) && name.endsWith(suffix);
+		}
+	});
 }
+
+
+private static void copyFile(File source, File dest) throws IOException {
+    InputStream is = null;
+    OutputStream os = null;
+    try {
+        is = new FileInputStream(source);
+        os = new FileOutputStream(dest);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = is.read(buffer)) > 0) {
+            os.write(buffer, 0, length);
+        }
+    } finally {
+        is.close();
+        os.close();
+    }
+}
+}
+
