1b682f3dc0c6fd0f10d6337342cb080b619093cb
Gabor <gabor.gyorkei@gmail.com>
2018-11-09 13:45:56 +0000

    Removed some legacy classes.

--- planner/main/src/uk/ac/ox/cs/pdq/planner/dag/explorer/parallel/Context.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.ox.cs.pdq.planner.dag.explorer.parallel;
-
-/**
- * Passes information to IterativeExecutor objects.
- * @author Efthymia Tsamoura
- *
- */
-public interface Context {
-}
