4e8e15ac529bd41d75f0356d1c49fc45bc950181
Gabor <gabor.gyorkei@gmail.com>
2018-11-22 15:12:09 +0000

    Updated comments and removed commented out code.

--- planner/main/src/uk/ac/ox/cs/pdq/planner/dominance/CostDominance.java
+++ planner/main/src/uk/ac/ox/cs/pdq/planner/dominance/CostDominance.java
@@ -7,24 +7,23 @@ import uk.ac.ox.cs.pdq.cost.Cost;
 import uk.ac.ox.cs.pdq.cost.estimators.OrderIndependentCostEstimator;
 
 /**
-* Closed cost dominance. 
-* A closed plan p cost dominates another closed plan p', if p is closed and has cost < the cost of p'.
-*
-* @author Efthymia Tsamoura
-* @author Gabor
-*/
+ * Closed cost dominance. A closed plan p cost dominates another closed plan p',
+ * if p is closed and has cost < the cost of p'.
+ *
+ * @author Efthymia Tsamoura
+ * @author Gabor
+ */
 
 public class CostDominance {
-
-	//	/**  True if we use a simple cost function to compare plans. */
-	//	private final boolean simpleFunction;
 
 	/** The estimator. */
 	private final OrderIndependentCostEstimator costEstimatorForOpenPlans;
 
 	/**
 	 * Constructor for SuccessDominance.
-	 * @param simpleFunction Boolean
+	 * 
+	 * @param simpleFunction
+	 *            Boolean
 	 */
 	public CostDominance(OrderIndependentCostEstimator estimator) {
 		Preconditions.checkNotNull(estimator);
@@ -34,15 +33,17 @@ public class CostDominance {
 	/**
 	 * Checks if is dominated.
 	 *
-	 * @param source the source
-	 * @param target the target
+	 * @param source
+	 *            the source
+	 * @param target
+	 *            the target
 	 * @return true if the source plan is success dominated by the target
 	 */
 	public boolean isDominated(RelationalTerm source, Cost sourceCost, RelationalTerm target, Cost targetCost) {
-		if(source.isClosed() && target.isClosed()) 
+		if (source.isClosed() && target.isClosed())
 			return sourceCost.greaterThan(targetCost);
-		
-		return this.costEstimatorForOpenPlans.cost(source).greaterThan(this.costEstimatorForOpenPlans.cost(target)); 
+
+		return this.costEstimatorForOpenPlans.cost(source).greaterThan(this.costEstimatorForOpenPlans.cost(target));
 	}
 
 	/**
--- planner/main/src/uk/ac/ox/cs/pdq/planner/dominance/package-info.java
+++ planner/main/src/uk/ac/ox/cs/pdq/planner/dominance/package-info.java
@@ -4,17 +4,15 @@ package uk.ac.ox.cs.pdq.planner.dominance;
 	@author Efthymia Tsamoura
 
 	The classes of this package implement different notions of configuration and plan dominance:
-	
-	-Closed dominance. A closed configuration c dominates a closed configuration c', if c both cost- and fact- dominates c'.
-	-Closed success dominance. A closed plan p success dominates another closed plan p', if p is successful and has cost < the cost of p'.
-	-Fact dominance. A configuration c and c' is fact dominated by another configuration c' if there exists an homomorphism from the facts of c to the facts of c' and
-	the input constants are preserved.
-	-Numerical success dominance. A configuration c dominates a configuration c', if c has >= facts than c'.
-	-Open success dominance. A plan p success dominates another closed plan ', if p is successful and has cost < the cost of p'.
- 	If either of the plans is open, then a simple plan cost estimator is used to assess their cost; otherwise, the costs of their corresponding (closed) plans are considered.
- 	-Open configuration dominance. A configuration c dominates a configuration c',
- 	if c both cost- and fact- dominates c' with one of the two being strict.
- 	When both configurations are open, then a simple plan cost estimator is used
- 	to assess the configurations' costs; otherwise, the costs of their corresponding (closed) plans are considered.
-
+	<pre>
+	 - CostDominance: Closed cost dominance. A closed plan p cost dominates another closed plan p', if p is closed and has cost < the cost of p'.
+	 - CostFactDominance: Closed dominance. A closed configuration c dominates a closed configuration c', if c both cost- and fact- dominates c'.
+ 	 - FactDominance: A configuration c and c' is fact dominated by another configuration c' if there exists an homomorphism from the facts of c to the facts of c' and
+ 			the input constants are preserved.
+ 	 - FastFactDominance: Performs fast fact dominance checks. A source configuration is fact dominated
+ 			by a target configuration if any inferred accessible fact plus in the source
+ 			configuration also appears in the target configuration. In order to perform
+ 			this kind of check Skolem constants must be assigned to formula variables
+ 			during chasing.
+	- NumericalFactDominance: A configuration c dominates a configuration c', if c has >= facts than c'.
 **/
