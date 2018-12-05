b6dd834887bc73092870658c6926bd99770fb727
marler <marler@clpc463.cs.ox.ac.uk>
2018-12-05 12:54:53 +0000

    try catch around the whole of parsing

--- demo/main/src/uk/ac/ox/cs/pdq/ui/io/sql/SQLLikeQueryReader.java
+++ demo/main/src/uk/ac/ox/cs/pdq/ui/io/sql/SQLLikeQueryReader.java
@@ -74,9 +74,140 @@ public class SQLLikeQueryReader {
         SQLiteSimpleListener listener = new SQLiteSimpleListener();
         SQLiteErrorListener elistener = new SQLiteErrorListener();
         
+        ConjunctiveQuery query;
         try
         {
         	parser.parse();
+                
+        	// Parse the variables from the SELECT ... FROM statement
+        	ArrayList<Variable> list = new ArrayList<>();
+        	for(String columnName : parser.getColumnNames()) {
+        		list.add(new Variable(columnName));
+        	}
+
+        	Term[] terms = new Term[list.size()];
+        	int i = 0;
+        	for(Variable var : list) terms[i++] = var;
+
+        	// Collect relation names and aliases:       
+        	this.aliasToTables = new HashMap<String, String>();
+        	for(String raw : parser.getTableAliases()) {
+        		if(raw.contains("AS"))
+        		{
+        			String[] elements = raw.split("AS");
+        			String tableName = elements[0].replaceAll("\\s","");
+        			String aliasName = elements[1].replaceAll("\\s","");
+        			this.aliasToTables.put(aliasName, tableName);
+        		}
+        	}
+
+        	log.debug("query aliases: " + this.aliasToTables);
+
+        	// Initialise the Arnold Schwarzenegger Conjunctive Query Body Builder
+        	ConjunctiveQueryBodyBuilder qBuilder = new ConjunctiveQueryBodyBuilder(this.schema, this.aliasToTables, terms);
+
+        	List<String> joinConstraints = new ArrayList<String>();
+        	for(String s : parser.getJoinConstraints() ) {
+        		String[] individualConstraints = s.split("AND");
+
+        		for( String constraint : individualConstraints ) {
+        			joinConstraints.add(constraint);
+        		}
+        	}
+
+        	// Begin joining on join constraints
+        	for( String constraint : joinConstraints ) {
+        		String[] elements = constraint.split("=");
+        		String left = elements[0];
+        		String right = elements[1];
+
+        		String leftAlias = left.split("\\.")[0].replaceAll("\\s", "");
+        		String leftColumnName = left.split("\\.")[1].replaceAll("\\s", "");
+        		String rightAlias = right.split("\\.")[0].replaceAll("\\s", "");
+        		String rightColumnName = right.split("\\.")[1].replaceAll("\\s", "");
+
+        		ConjunctiveQueryBodyBuilder.AliasAttrConstraintTerm leftAliasAttr =
+        				new ConjunctiveQueryBodyBuilder.AliasAttrConstraintTerm(leftAlias, leftColumnName);
+        		ConjunctiveQueryBodyBuilder.AliasAttrConstraintTerm rightAliasAttr =
+        				new ConjunctiveQueryBodyBuilder.AliasAttrConstraintTerm(rightAlias, rightColumnName);
+
+        		qBuilder.addConstraint(leftAliasAttr, rightAliasAttr);
+        	} 
+
+
+        	// Begin adding where constraints:
+        	String rawExpression = parser.getLastExpr();
+        	if ((rawExpression != null) && (!rawExpression.equals(""))) {
+        		log.debug("raw where expr: " + rawExpression);
+        		String[] rawConstraints = rawExpression.split("AND");
+
+        		for( String rawConstraint : rawConstraints ) {
+        			if(rawConstraint.split("=").length >= 2)
+        			{
+        				String left = rawConstraint.split("=")[0].replaceAll("\\s", "");
+        				String right = rawConstraint.split("=")[1].replaceAll("\\s", "");
+
+        				ConjunctiveQueryBodyBuilder.ConstraintTerm leftConstraint = null;
+        				ConjunctiveQueryBodyBuilder.ConstraintTerm rightConstraint = null;
+
+        				// Process left:
+        				if(left.contains("'")) {
+        					String constant = left.replaceAll("'", "");
+
+        					leftConstraint = new ConjunctiveQueryBodyBuilder.ConstantConstraintTerm(constant);
+        				} else if( left.contains(".") ) {
+        					String aliasName = left.split("\\.")[0];
+        					String attrName = left.split("\\.")[1];
+
+        					leftConstraint = new ConjunctiveQueryBodyBuilder.AliasAttrConstraintTerm(aliasName, attrName);
+        				}
+        				else
+        				{
+        					leftConstraint = new ConjunctiveQueryBodyBuilder.ConstantConstraintTerm(left);
+        				}
+
+        				// Process right:
+        				if(right.contains("'")) {
+        					String constant = right.replaceAll("'", "");
+
+        					rightConstraint = new ConjunctiveQueryBodyBuilder.ConstantConstraintTerm(constant);
+        				} else if( right.contains(".") ) {
+        					String aliasName = right.split("\\.")[0];
+        					String attrName = right.split("\\.")[1];
+
+        					rightConstraint = new ConjunctiveQueryBodyBuilder.AliasAttrConstraintTerm(aliasName, attrName);
+        				}
+        				else
+        				{
+        					rightConstraint = new ConjunctiveQueryBodyBuilder.ConstantConstraintTerm(right);
+        				}
+        				qBuilder.addConstraint(leftConstraint, rightConstraint);
+        			}
+        		}
+        	}
+
+        	// Build the query head:
+        	for (String rawResultColumn : parser.getResultColumns() ) {
+
+        		if( rawResultColumn.equals("*") ) {
+        			qBuilder.returnAllVars();
+        		} else {
+        			if(rawResultColumn.contains("."))
+        			{
+        				String aliasName = rawResultColumn.split("\\.")[0];
+        				String attrName = rawResultColumn.split("\\.")[1];
+
+        				qBuilder.addResultColumn(aliasName, attrName);
+        			}
+        		}
+        	}
+
+            log.debug("qBuilder: " + qBuilder);
+        	
+        	// Call toConjunctiveQuery() as the last step
+        	query = qBuilder.toConjunctiveQuery();
+        	
+        	log.debug("query: " + query);
         }
         catch(Exception e)
         {
@@ -87,136 +218,7 @@ public class SQLLikeQueryReader {
         	alert.showAndWait();
         	throw e;
         }
-                
-        // Parse the variables from the SELECT ... FROM statement
-    	ArrayList<Variable> list = new ArrayList<>();
-    	for(String columnName : parser.getColumnNames()) {
-        	list.add(new Variable(columnName));
-        }
-
-    	Term[] terms = new Term[list.size()];
-        int i = 0;
-        for(Variable var : list) terms[i++] = var;
-               
-        // Collect relation names and aliases:       
-        this.aliasToTables = new HashMap<String, String>();
-        for(String raw : parser.getTableAliases()) {
-        	if(raw.contains("AS"))
-        	{
-        		String[] elements = raw.split("AS");
-        		String tableName = elements[0].replaceAll("\\s","");
-        		String aliasName = elements[1].replaceAll("\\s","");
-        		this.aliasToTables.put(aliasName, tableName);
-        	}
-        }
         
-        log.debug("query aliases: " + this.aliasToTables);
-        
-        // Initialise the Arnold Schwarzenegger Conjunctive Query Body Builder
-        ConjunctiveQueryBodyBuilder qBuilder = new ConjunctiveQueryBodyBuilder(this.schema, this.aliasToTables, terms);
-  
-        List<String> joinConstraints = new ArrayList<String>();
-        for(String s : parser.getJoinConstraints() ) {
-        	String[] individualConstraints = s.split("AND");
-        	
-        	for( String constraint : individualConstraints ) {
-        		joinConstraints.add(constraint);
-        	}
-        }
-        
-        // Begin joining on join constraints
-    	for( String constraint : joinConstraints ) {
-        	String[] elements = constraint.split("=");
-        	String left = elements[0];
-        	String right = elements[1];
-        	
-        	String leftAlias = left.split("\\.")[0].replaceAll("\\s", "");
-        	String leftColumnName = left.split("\\.")[1].replaceAll("\\s", "");
-        	String rightAlias = right.split("\\.")[0].replaceAll("\\s", "");
-        	String rightColumnName = right.split("\\.")[1].replaceAll("\\s", "");
-        	
-        	ConjunctiveQueryBodyBuilder.AliasAttrConstraintTerm leftAliasAttr =
-        			new ConjunctiveQueryBodyBuilder.AliasAttrConstraintTerm(leftAlias, leftColumnName);
-        	ConjunctiveQueryBodyBuilder.AliasAttrConstraintTerm rightAliasAttr =
-        			new ConjunctiveQueryBodyBuilder.AliasAttrConstraintTerm(rightAlias, rightColumnName);
-        	
-        	qBuilder.addConstraint(leftAliasAttr, rightAliasAttr);
-        } 
-    	
-    	
-    	// Begin adding where constraints:
-		String rawExpression = parser.getLastExpr();
-		if ((rawExpression != null) && (!rawExpression.equals(""))) {
-        	log.debug("raw where expr: " + rawExpression);
-        	String[] rawConstraints = rawExpression.split("AND");
-        	
-        	for( String rawConstraint : rawConstraints ) {
-        	if(rawConstraint.split("=").length >= 2)
-        	{
-        		String left = rawConstraint.split("=")[0].replaceAll("\\s", "");
-        		String right = rawConstraint.split("=")[1].replaceAll("\\s", "");
-        		
-        		ConjunctiveQueryBodyBuilder.ConstraintTerm leftConstraint = null;
-        		ConjunctiveQueryBodyBuilder.ConstraintTerm rightConstraint = null;
-        		
-        		// Process left:
-        		if(left.contains("'")) {
-         			String constant = left.replaceAll("'", "");
-         			
-         			leftConstraint = new ConjunctiveQueryBodyBuilder.ConstantConstraintTerm(constant);
-         		} else if( left.contains(".") ) {
-        			String aliasName = left.split("\\.")[0];
-        			String attrName = left.split("\\.")[1];
-        			
-        			leftConstraint = new ConjunctiveQueryBodyBuilder.AliasAttrConstraintTerm(aliasName, attrName);
-        		}
-         		else
-         		{
-         			leftConstraint = new ConjunctiveQueryBodyBuilder.ConstantConstraintTerm(left);
-         		}
-        		
-        		// Process right:
-         		if(right.contains("'")) {
-         			String constant = right.replaceAll("'", "");
-         			
-         			rightConstraint = new ConjunctiveQueryBodyBuilder.ConstantConstraintTerm(constant);
-         		} else if( right.contains(".") ) {
-        			String aliasName = right.split("\\.")[0];
-        			String attrName = right.split("\\.")[1];
-        			
-        			rightConstraint = new ConjunctiveQueryBodyBuilder.AliasAttrConstraintTerm(aliasName, attrName);
-        		}
-         		else
-         		{
-         			rightConstraint = new ConjunctiveQueryBodyBuilder.ConstantConstraintTerm(right);
-         		}
-        		qBuilder.addConstraint(leftConstraint, rightConstraint);
-        	}
-        	}
-		}
-    	
-    	// Build the query head:
-    	for (String rawResultColumn : parser.getResultColumns() ) {
-    		
-    		if( rawResultColumn.equals("*") ) {
-  			qBuilder.returnAllVars();
-    		} else {
-    			if(rawResultColumn.contains("."))
-    			{
-    				String aliasName = rawResultColumn.split("\\.")[0];
-    				String attrName = rawResultColumn.split("\\.")[1];
-    			
-    				qBuilder.addResultColumn(aliasName, attrName);
-    			}
-    		}
-    	}
-    	
-    	log.debug("qBuilder: " + qBuilder);
-    	
-    	// Call toConjunctiveQuery() as the last step
-    	ConjunctiveQuery query = qBuilder.toConjunctiveQuery();
-    	
-    	log.debug("query: " + query);
     	
         return query;
 	}
