c7c0d5386eec71aad4090634ce39336869889979
michaelbenedikt <michael.benedikt@gmail.com>
2019-01-07 13:05:25 +0000

    update package info

--- planner/main/src/uk/ac/ox/cs/pdq/planner/dag/explorer/filters/package-info.java
+++ planner/main/src/uk/ac/ox/cs/pdq/planner/dag/explorer/filters/package-info.java
@@ -1,7 +1,7 @@
 package uk.ac.ox.cs.pdq.planner.dag.explorer.filters;
 
 /**
- * @author Efthymia Tsamoura
+ * @author Efthymia Tsamoura and Michael Benedikt
  * This package contains classes that given a set of DAG configurations 
  * they filter out the configurations that do not satisfy certain criteria. 
  * Currently there is only one filter, which filters out a configuration C1 if 
