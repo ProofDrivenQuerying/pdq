635ca19a7aac54662b6b9d3a5f8eacadf74feafc
Gabor <gabor.gyorkei@gmail.com>
2019-01-09 10:19:12 +0000

    Test created to visualise 2 different plans. Helps to identify differences in the result plans.

---
+++ regression/main/src/uk/ac/ox/cs/pdq/regression/utils/PlaanPrinterTest.java
@@ -0,0 +1,30 @@
+package uk.ac.ox.cs.pdq.regression.utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.bind.JAXBException;
+
+import uk.ac.ox.cs.pdq.algebra.RelationalTerm;
+import uk.ac.ox.cs.pdq.cost.io.jaxb.CostIOManager;
+import uk.ac.ox.cs.pdq.db.Schema;
+import uk.ac.ox.cs.pdq.io.PlanPrinter;
+import uk.ac.ox.cs.pdq.io.jaxb.IOManager;
+
+public class PlaanPrinterTest {
+
+	public PlaanPrinterTest() {
+	}
+
+	public static void main(String[] args) throws IOException, InterruptedException, JAXBException {
+		File planFileNew = new File("..//regression//TestResults//newResults.xml");
+		File planFileOld = new File("..//regression//TestResults//oldResults.xml");
+		Schema schema = IOManager
+				.importSchema(new File("..//regression//test//planner//linear//fast//demo//case_001//schema.xml"));
+		RelationalTerm old = CostIOManager.readRelationalTermFromRelationaltermWithCost(planFileOld, schema );
+		RelationalTerm newPlan= CostIOManager.readRelationalTermFromRelationaltermWithCost(planFileNew, schema );
+		PlanPrinter.openPngPlan(old);
+		PlanPrinter.openPngPlan(newPlan);
+	}
+
+}
