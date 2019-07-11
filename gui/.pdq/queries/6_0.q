a98c9438e290cfe2b1cc9dd01ed030196ab9d6ad
marler <marler@CLPC463.cs.ox.ac.uk>
2019-07-11 12:32:45 +0100

    Cut-down version of 2.s and 2_1.q

---
+++ gui/.pdq/queries/6_0.q
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<query name="\queries\2_1.q" description="" type="conjunctive">
+<body>
+<atom name="YahooWeather">
+<variable name="_1"/>
+<variable name="city"/>
+<variable name="country2"/>
+<variable name="region"/>
+<variable name="distance_unit"/>
+<variable name="pressure_unit"/>
+<variable name="speed_unit"/>
+<variable name="temp_unit"/>
+<variable name="wind_chill"/>
+<variable name="wind_direction"/>
+<variable name="wind_speed"/>
+<variable name="humidity"/>
+<variable name="pressure"/>
+<variable name="rising"/>
+<variable name="visibility"/>
+<variable name="sunrise"/>
+<variable name="sunset"/>
+<variable name="date"/>
+<variable name="temperature"/>
+<constant value="Sunny"/>
+<constant value="FR"/>
+</atom>
+<atom name="YahooPlaceCode">
+<constant value="iso"/>
+<constant value="FR"/>
+<variable name="_1"/>
+</atom>
+</body>
+<head name="Q">
+</head>
+</query>
---
+++ gui/.pdq/schemas/6.s
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<schema name="Yahoo! API" description="">
+<relations>
+<relation name="YahooPlaceCode" source="yahoo" size="10000000">
+		<attribute name="namespace" type="java.lang.String"  input-method="relation.1"/>
+		<attribute name="code"      type="java.lang.String"  input-method="relation.2"/>
+		<attribute name="woeid"     type="java.lang.Integer" path="woeid"/>
+		<access-method name="yh_geo_code" type="LIMITED" inputs="0,1" cost="1.0"/>
+</relation>
+<relation name="YahooWeather" source="yahoo" size="100000000">
+		<attribute name="woeid"          type="java.lang.Integer" input-method="q.2" />
+
+		<attribute name="city"           type="java.lang.String"  path="location/city"/>
+		<attribute name="country2"        type="java.lang.String"  path="location/country"/>
+		<attribute name="region"         type="java.lang.String"  path="location/region"/>
+
+		<attribute name="distance_unit"  type="java.lang.String"  path="units/distance"/>
+		<attribute name="pressure_unit"  type="java.lang.String"  path="units/pressure"/>
+		<attribute name="speed_unit"     type="java.lang.String"  path="units/speed"/>
+		<attribute name="temp_unit"      type="java.lang.String"  path="units/temperature"/>
+
+		<attribute name="wind_chill"     type="java.lang.Integer" path="wind/chill"/>
+		<attribute name="wind_direction" type="java.lang.Integer" path="wind/direction"/>
+		<attribute name="wind_speed"     type="java.lang.String"  path="wind/speed"/>
+
+		<attribute name="humidity"       type="java.lang.Double"  path="atmosphere/humidity"/>
+		<attribute name="pressure"       type="java.lang.Double"  path="atmosphere/pressure"/>
+		<attribute name="rising"         type="java.lang.Integer" path="atmosphere/rising"/>
+		<attribute name="visibility"     type="java.lang.Double"  path="atmosphere/visibility"/>
+
+		<attribute name="sunrise"        type="java.lang.String"  path="astronomy/sunrise"/>
+		<attribute name="sunset"         type="java.lang.String"  path="astronomy/sunset"/>
+
+		<attribute name="date"           type="java.lang.String"  path="item/condition/date"/>
+		<attribute name="temperature"    type="java.lang.Double"  path="item/condition/temp"/>
+		<attribute name="condition"      type="java.lang.String"  path="item/condition/text"/>
+		<attribute name="code"           type="java.lang.Integer" path="item/condition/code"/>
+<access-method name="yh_wtr_woeid" type="LIMITED" inputs="0" cost="10.0"/>
+</relation>
+</relations>
+<dependencies>
+</dependencies>
+</schema>
