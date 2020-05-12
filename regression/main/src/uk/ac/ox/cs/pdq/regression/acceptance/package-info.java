8a8fa6e8bc6593eda533bee477d02d2a1af923f2
michaelbenedikt <michaelbenedikt@users.noreply.github.com>
2020-05-12 14:16:31 -0400

    Update package-info.java
    write real summary of package

--- regression/main/src/uk/ac/ox/cs/pdq/regression/acceptance/package-info.java
+++ regression/main/src/uk/ac/ox/cs/pdq/regression/acceptance/package-info.java
@@ -1,24 +1,9 @@
 package uk.ac.ox.cs.pdq.regression.acceptance;
 
 /**
-	@author Mark Ridler
+	@author Michael Benedikt
 	
-	This package contains:
-	
-	- AcceptanceCriterion.java
- 		* High-level acceptance criterion.
-	- ApproximateCostAcceptanceCheck.java
- 		* Acceptance test request the expected plan cost to be within an order of 
- 		* magnitude of the expected plan cost to pass.
-	- ExpectedCardinalityAcceptanceCheck.java
- 		* Acceptance test request the expected and observed results to be equivalent
- 		* under set semantics.
-	- SameCostAcceptanceCheck.java
- 		* Acceptance test request the expected and observed plans to have the same cost
- 		* to pass.
-	- SetEquivalentResultSetsAcceptanceCheck.java
- 		* Acceptance test request the expected and observed results to be equivalent
- 		* under set semantics.
+	This package contains various criteria for a test to pass, which can be used in the regression package (e.g. test should pass if the 
+results are the same underlying set, ignoring order)
 
-	This package runs regression tests with a theme of acceptance criteria.
 **/
