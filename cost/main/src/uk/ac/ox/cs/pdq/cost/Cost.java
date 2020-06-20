db1f6a8326b7b662ba9db910b7c2d65e84507760
michaelbenedikt <michaelbenedikt@users.noreply.github.com>
2020-06-20 13:12:33 -0400

    Update Cost.java
    eliminate more BS pseudo-comments

--- cost/main/src/uk/ac/ox/cs/pdq/cost/Cost.java
+++ cost/main/src/uk/ac/ox/cs/pdq/cost/Cost.java
@@ -13,7 +13,7 @@ import uk.ac.ox.cs.pdq.cost.io.jaxb.adapters.CostAdapter;
 public abstract class Cost implements Comparable<Cost>{
 	
 	/**
-	 * Less or equals.
+	 * 
 	 *
 	 * @param cost Input cost object
 	 * @return true if this.cost <= cost
@@ -21,7 +21,7 @@ public abstract class Cost implements Comparable<Cost>{
 	public abstract boolean lessOrEquals(Cost cost);
 
 	/**
-	 * Less than.
+	 * 
 	 *
 	 * @param cost Input cost object
 	 * @return true if this.cost < cost
@@ -29,7 +29,7 @@ public abstract class Cost implements Comparable<Cost>{
 	public abstract boolean lessThan(Cost cost);
 
 	/**
-	 * Greater or equals.
+	 * 
 	 *
 	 * @param cost Input cost object
 	 * @return true if this.cost >= cost
@@ -37,7 +37,7 @@ public abstract class Cost implements Comparable<Cost>{
 	public abstract boolean greaterOrEquals(Cost cost);
 
 	/**
-	 * Greater than.
+	 * 
 	 *
 	 * @param cost Input cost object
 	 * @return true if this.cost > cost
@@ -45,7 +45,7 @@ public abstract class Cost implements Comparable<Cost>{
 	public abstract boolean greaterThan(Cost cost);
 
 	/**
-	 * Gets the value.
+	 * 
 	 *
 	 * @return a numeric representation of the cost
 	 */
