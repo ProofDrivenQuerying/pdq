bcd3f0fc3b60b35309cbcaf80f5ac6f2803ae2c3
michaelbenedikt <michaelbenedikt@users.noreply.github.com>
2020-06-25 05:09:33 -0400

    Update package-info.java

--- datasources/test/src/uk/ac/ox/cs/pdq/test/datasources/simplewebservice/package-info.java
+++ datasources/test/src/uk/ac/ox/cs/pdq/test/datasources/simplewebservice/package-info.java
@@ -1,8 +1,8 @@
 package uk.ac.ox.cs.pdq.test.datasources.simplewebservice;
 
 /**
-	@author Gabor
+	@author Michael Benedikt
 	
-	This package contains a test of both the older "service" specification and the newer "simple web service" specification, using two versions of the same service.
+	This package contains a test of both the older "service" specification and the newer "simple web service" specification; one uses an EBI-based service and another a toy service hosted at Oxford
 
 **/
