b844f05ffb52ebe12d64b38b6b847faf8a480d93
michaelbenedikt <michaelbenedikt@users.noreply.github.com>
2020-06-28 15:17:41 -0400

    Update package-info.java

--- datasources/main/src/uk/ac/ox/cs/pdq/datasources/memory/package-info.java
+++ datasources/main/src/uk/ac/ox/cs/pdq/datasources/memory/package-info.java
@@ -1,12 +1,8 @@
 package uk.ac.ox.cs.pdq.datasources.memory;
 
 /**
-	@author Mark Ridler
+	@author Michael Benedikt
 	
-	This package contains:
+	This package contains an implementation of an access that just reads data from main memory
 	
-	- InMemoryAccessMethod.java
- 		* Represents an in-memory version of an executable access method from which it is
- 		* derived.
-
 **/
