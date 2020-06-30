0b8f906e94ff41bbb98cf42c80ed73f3759c87cb
Fergus Cooper <fcooper8472@gmail.com>
2020-06-30 22:55:36 +0100

    Rename FullMain -> GuiMain

---
+++ gui/main/src/uk/ac/ox/cs/pdq/ui/GuiMain.java
@@ -0,0 +1,15 @@
+// This file is part of PDQ (https://github.com/ProofDrivenQuerying/pdq) which is released under the MIT license.
+// See accompanying LICENSE for copyright notice and full details.
+
+package uk.ac.ox.cs.pdq.ui;
+
+/**
+ * FullMain
+ */
+public class GuiMain {
+
+    public static void main(String[] args) {
+        PDQApplication.main(args);
+    }
+
+}
