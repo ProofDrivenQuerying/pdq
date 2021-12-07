921f174bdaa1a5c45ee9dac7379d8e20fbd13d1e
michaelbenedikt <michaelbenedikt@users.noreply.github.com>
2021-12-07 15:11:21 +0000

    Create package-info.java

---
+++ gui/main/src/uk/ac/ox/cs/pdq/ui/io/package-info.java
@@ -0,0 +1,3 @@
+The SQL parser files are for parsing queries that a user inputs or modifies when the GUI is running.
+
+The other IO files deal with reading schema or query information that is persisted from prior sessions.
