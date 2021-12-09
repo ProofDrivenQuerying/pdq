cb85ac9d3989da6658d89cda4c05c7d0fc2a2b95
michaelbenedikt <michaelbenedikt@users.noreply.github.com>
2021-12-09 17:24:15 +0000

    Update package-info.java

--- datasources/main/src/uk/ac/ox/cs/pdq/datasources/services/service/package-info.java
+++ datasources/main/src/uk/ac/ox/cs/pdq/datasources/services/service/package-info.java
@@ -4,7 +4,7 @@
 package uk.ac.ox.cs.pdq.datasources.services.service;
 
 // This package contains the objects which are used to parse in JAXB the service as an Xml file. It contains
-// the Service itself which contains ServiceUsagePolicy, StaticAttribute and RESTExecutableAccessMethodSpecification.
+// the Service itself which contains  StaticAttribute and RESTExecutableAccessMethodSpecification.
 // RESTExecutableAccessMethodSpecification in turn contains RESTExecutableAccessMethodAttributeSpecification.
 //
 // Mark Ridler
